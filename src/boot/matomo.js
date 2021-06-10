590de09f5dde71169526820e101a03ea4e53b186
lukegravitt <lukegravitt@gmail.com>
2021-06-10 13:27:20 -0700

    Revert "Use different Matomo id for test/production"
    This reverts commit 8e55cd69d895170b9348ea015e46fe02d1a4d10d.

--- src/boot/matomo.js
+++ src/boot/matomo.js
@@ -3,7 +3,7 @@ import VueMatomo from 'vue-matomo'
 export default ({ Vue, router }) => {
   Vue.use(VueMatomo, {
     host: 'https://track.hypha.earth/',
-    siteId: process.env.NODE_ENV === 'production' ? 1 : 2,
+    siteId: 1,
     router
   })
 }
