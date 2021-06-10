8e55cd69d895170b9348ea015e46fe02d1a4d10d
lukegravitt <lukegravitt@gmail.com>
2021-06-10 13:06:32 -0700

    Use different Matomo id for test/production

--- src/boot/matomo.js
+++ src/boot/matomo.js
@@ -3,7 +3,7 @@ import VueMatomo from 'vue-matomo'
 export default ({ Vue, router }) => {
   Vue.use(VueMatomo, {
     host: 'https://track.hypha.earth/',
-    siteId: 1,
+    siteId: process.env.NODE_ENV === 'production' ? 1 : 2,
     router
   })
 }
