6cdfa767ade2e7161fd3ab992d3bb73e8fd6b0b8
lukegravitt <lukegravitt@gmail.com>
2021-07-14 13:31:24 -0700

    Update navigation and pages to latest review

---
+++ src/pages/dho/Overview.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  name: 'dho-overview',
+  components: {
+    Widget: () => import('~/components/common/widget.vue')
+  }
+}
+</script>
+
+<template lang="pug">
+.dho-overview.q-px-xl
+  .row.items-center.justify-between.q-mb-md
+    .text-h4 {{ 'About Hypha' }}
+    q-btn(icon="fas fa-cog" label="Admin")
+  .row.q-col-gutter-md
+    .col-3
+      widget(title="Circle One")
+        .text-body2 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        .text-center.q-my-md
+          .text-h2 15
+          .text-bold active works
+        .row.items-center.justify-between
+          .row
+            q-avatar(size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+          .text-subtitle2.text-italic and other 2
+    .col-3
+      widget(title="Circle Two")
+        text-body2 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        .text-center.q-my-md
+          .text-h2 8
+          .text-bold active works
+        .row.items-center.justify-between
+          .row
+            q-avatar(size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+          .text-subtitle2.text-italic and other 12
+    .col-3
+      widget(title="Circle Three")
+        text-body2 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        .text-center.q-my-md
+          .text-h2 54
+          .text-bold active works
+        .row.items-center.justify-between
+          .row
+            q-avatar(size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+          .text-subtitle2.text-italic and other 21
+    .col-3
+      widget(title="Circle Four")
+        text-body2 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        .text-center.q-my-md
+          .text-h2 23
+          .text-bold active works
+        .row.items-center.justify-between
+          .row
+            q-avatar(size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+            q-avatar(:style="{ 'margin-left': '-8px' }" size="sm")
+              img(src="avatar-placeholder.png")
+          .text-subtitle2.text-italic and other 35
+</template>
