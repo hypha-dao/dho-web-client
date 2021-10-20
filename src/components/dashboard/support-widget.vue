92794d0cf940a42931e3bdfe09f41ff8013dbd79
Jose <jmgayosso@mightywizards.com>
2021-10-20 14:21:56 -0500

    Implemeted redirect to support Hypha channel

--- src/components/dashboard/support-widget.vue
+++ src/components/dashboard/support-widget.vue
@@ -12,7 +12,7 @@ widget.full-height(title="Need support?")
   .text-caption.q-pl-sm.q-mt-lg Lorem ipsum dolor sit amet, consectetur adipiscing
   .row.justify-between.q-mt-lg
     .col-auto
-      q-btn.q-mt-md.q-px-sm.text-white(rounded color="primary" type="a" href="https://notepad.hypha.earth/5dC66nNXRVGpb1aTHaRJXw" target="blank") Documentation
+      q-btn.q-mt-md.q-px-sm.text-white(rounded color="primary" type="a" href="https://notepad.hypha.earth/5dC66nNXRVGpb1aTHaRJXw" target="_blank") Documentation
     .col
-      q-btn.q-mt-md.q-ml-sm(round color="primary" icon="fab fa-discord" size="0.7rem")
+      q-btn.q-mt-md.q-ml-sm(round color="primary" icon="fab fa-discord" size="0.7rem" type="a" href="https://discord.com/channels/722537361480613950/732285564384051323" target="_blank")
 </template>
