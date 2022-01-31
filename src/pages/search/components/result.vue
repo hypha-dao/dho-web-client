d605b1efeca81716c1eae22e75df0937a311c234
DaniPoot <danielpoot.uc2@gmail.com>
2022-01-31 09:10:30 -0600

    Create component for result

---
+++ src/pages/search/components/result.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+widget.bg-grey-4.q-mb-sm
+  .row.items-center.justify-between
+    q-btn(round unelevated :icon="icon" color="primary" text-color="white" size="sm" :ripple="false")
+    .q-ml-md.q-mr-auto
+      .text-body1.text-bold {{ title }}
+      .text-body2.text-italic.grey-color {{ type }}
+    chips(:tags="tags")
+</template>
+
+<script>
+export default {
+  name: 'result',
+  components: {
+    Widget: () => import('~/components/common/widget.vue'),
+    Chips: () => import('~/components/common/chips.vue')
+  },
+  props: {
+    icon: String,
+    title: String,
+    type: String,
+    tags: Array
+  }
+}
+</script>
+
+<style scope lang="stylus">
+.grey-color
+  color: #84878E
+</style>
