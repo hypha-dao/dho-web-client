cc7351e730e453e59743e53d1f906f0004eb6ebb
DaniPoot <danielpoot.uc2@gmail.com>
2022-02-14 13:09:13 -0600

    Add news tags for proposal type and modify labels

--- src/pages/proposals/ProposalDetail.vue
+++ src/pages/proposals/ProposalDetail.vue
@@ -126,21 +126,21 @@ export default {
       if (proposal) {
         if (proposal.__typename === 'Payout') {
           return [
-            { color: 'primary', label: 'Contribution' },
-            { color: 'primary', outline: true, label: 'Circle One' }
+            { color: 'primary', label: 'Generic Contribution' }
+            // { color: 'primary', outline: true, label: 'Circle One' }
           ]
         }
 
         if (proposal.__typename === 'Assignment' || proposal.__typename === 'Edit') {
           return [
-            { color: 'primary', label: 'Assignment' },
+            { color: 'primary', label: 'Role Assignment' },
             { color: 'primary', outline: true, label: 'Circle One' }
           ]
         }
 
         if (proposal.__typename === 'Assignbadge') {
           return [
-            { color: 'primary', label: 'Badge' },
+            { color: 'primary', label: 'Badge Assignment' },
             { color: 'primary', outline: true, label: 'Circle One' }
           ]
         }
@@ -148,6 +148,18 @@ export default {
         if (proposal.__typename === 'Suspend') {
           return [
             { color: 'primary', label: 'Suspension' }
+          ]
+        }
+
+        if (proposal.__typename === 'Role') {
+          return [
+            { color: 'primary', label: 'Role Archetype' }
+          ]
+        }
+
+        if (proposal.__typename === 'Badge') {
+          return [
+            { color: 'primary', label: 'Badge' }
           ]
         }
       }
