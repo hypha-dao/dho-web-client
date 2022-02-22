e53b456db69774b5bb583c2d7d8e40b03421daf2
Pugakn <ipugakn@gmail.com>
2022-02-22 12:58:42 -0600

    Reduced sidebar margins

--- src/components/navigation/profile-sidebar.vue
+++ src/components/navigation/profile-sidebar.vue
@@ -32,9 +32,9 @@ export default {
             q-btn(color="grey-4" text-color="primary" rounded unelevated size="sm" padding="12px" icon="fas fa-times" @click="$emit('close')")
         .h3.q-mt-md.q-px-lg(v-if="profile") {{ profile.name }}
         .b3.text-grey-6.q-px-lg(v-if="profile") {{ '@' + profile.username }}
-      sidebar-news(:username="profile.username" :daoName="daoName").q-mt-xl
+      sidebar-news(:username="profile.username" :daoName="daoName").q-mt-md
       //- quick-actions //- Commented for MVP
-      quick-links.q-py-sm(:username="profile.username").q-mt-xl
+      quick-links.q-py-sm(:username="profile.username").q-mt-md
 </template>
 
 <style lang="stylus" scoped>
