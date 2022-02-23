a212b6418b64e487eb405a481cac36f974d82663
AlejandroM1ghty <alejandro@m1ghty.io>
2022-02-23 10:43:15 -0600

    Missing doc Id

--- src/store/proposals/index.js
+++ src/store/proposals/index.js
@@ -352,13 +352,14 @@ export default {
             account: this.$config.contracts.dao,
             name: 'propose',
             data: {
-              dao_hash: rootState.dao.hash,
+              dao_id: rootState.dao.docId,
               proposer: rootState.accounts.account,
               proposal_type: 'assignbadge',
               content_groups: [content],
               publish: true
             }
           }]
+          console.log(JSON.parse(JSON.stringify(actions)))
           return this.$api.signTransaction(actions)
         }
 
