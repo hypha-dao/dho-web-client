1dcc8cef9a69b65110e6491818457b7c48aa7749
AlejandroM1ghty <alejandro@m1ghty.io>
2022-02-28 11:27:31 -0600

    Modified CSS styles

--- src/components/login/welcome-view.vue
+++ src/components/login/welcome-view.vue
@@ -7,9 +7,9 @@ export default {
 <template lang="pug">
 .full-width.full-height
   .absolute-center.full-width.q-pa-xl
-    .text-h4 Welcome to
-    .text-h3.text-bold Hypha
-    .text-body2.text-weight-thin.q-mt-lg.q-mb-lg.text-grey The DHO (Decentralized Human Oranization) is a framework to build your organization from the ground uo in an organic and participative way and together with others.
+    .h1-signup.color-primary Welcome to
+    .h1-signup.color-primary.text-bold Hypha
+    .b1-signup.color-secondary.text-weight-thin.q-mt-lg.q-mb-lg.text-grey The DHO (Decentralized Human Oranization) is a framework to build your organization from the ground uo in an organic and participative way and together with others.
     q-btn.full-width.q-mt-xl(
       label="Begin your journey"
       color="primary"
