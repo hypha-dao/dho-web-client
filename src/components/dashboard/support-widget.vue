950f9165590e454711637ebbf8d5fbda9db70b36
AlejandroM1ghty <alejandro@m1ghty.io>
2022-02-28 12:01:49 -0600

    Home dashboard reviewed

--- src/components/dashboard/support-widget.vue
+++ src/components/dashboard/support-widget.vue
@@ -9,7 +9,7 @@ export default {
 
 <template lang="pug">
 widget(title="Need support?")
-  .b2.q-mt-xl Please read our Documentation for more info. If you are stuck with a problem you can also reach out to us on discord in the "dho-support" channel.
+  .b2.q-mt-md Please read our Documentation for more info. If you are stuck with a problem you can also reach out to us on discord in the "dho-support" channel.
   .row.justify-between.custom-padding
     .col-auto
       q-btn.q-mt-md.q-px-sm.text-white(rounded color="primary" type="a" href="https://notepad.hypha.earth/5dC66nNXRVGpb1aTHaRJXw" target="_blank") Documentation
@@ -18,7 +18,7 @@ widget(title="Need support?")
 </template>
 <style lang="stylus" scoped>
 .custom-padding
-  margin-top: 60px
+  margin-top: 100px
 .discord-buttom
   margin-top: 20px
 </style>
