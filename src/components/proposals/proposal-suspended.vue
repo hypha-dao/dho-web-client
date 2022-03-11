da90fe63a1a6cd4b4f237f0729937db7d9f5293d
DaniPoot <danielpoot.uc2@gmail.com>
2022-03-11 08:09:44 -0600

    Create a new component for proposal suspended

---
+++ src/components/proposals/proposal-suspended.vue
@@ -0,0 +1,17 @@
+<script>
+/**
+ * Proposal status view shown when a proposal is suspended
+ */
+export default {
+  name: 'proposal-suspended'
+}
+</script>
+
+<template lang="pug">
+div
+  .row.q-my-xl.gt-sm
+    .text-body.text-disabled Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  .row.justify-center.q-my-lg.q-px-lg
+    q-btn.full-width(no-caps rounded color="white" text-color="primary" @click="$emit('publish')") Publish
+    q-btn.q-mt-md.full-width(no-caps rounded flat color="white" @click="$emit('changed')") I changed my mind
+</template>
