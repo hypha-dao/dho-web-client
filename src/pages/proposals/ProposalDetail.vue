f1979de16f856c8ca7cd3af315f00f9fd0504ad2
DaniPoot <danielpoot.uc2@gmail.com>
2022-03-18 17:31:41 -0600

    Modify the variables to calculate the unit value

--- src/pages/proposals/ProposalDetail.vue
+++ src/pages/proposals/ProposalDetail.vue
@@ -455,10 +455,12 @@ export default {
 
     voting (proposal) {
       if (proposal && Array.isArray(proposal.votetally) && proposal.votetally.length) {
+        const passCount = parseFloat(proposal.pass.count)
+        const failCount = parseFloat(proposal.fail.count)
         const abstain = parseFloat(proposal.votetally[0].abstain_votePower_a)
         const pass = parseFloat(proposal.votetally[0].pass_votePower_a)
         const fail = parseFloat(proposal.votetally[0].fail_votePower_a)
-        const unity = (pass + fail > 0) ? pass / (pass + fail) : 0
+        const unity = (passCount + failCount > 0) ? passCount / (passCount + failCount) : 0
         let supply = this.supply
         if (proposal.details_ballotQuorum_a) {
           const [amount] = proposal.details_ballotQuorum_a.split(' ')
