e683d775236100796528959eadfe72ef5a446cde
Arsenije Savić <arsenijesavic@me.com>
2022-03-23 11:15:16 -0600

    Add pick function

---
+++ src/utils/pick.js
@@ -0,0 +1,19 @@
+/*!
+ * Create a new object composed of properties picked from another object
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Object} obj   The object to pick properties from
+ * @param  {Array}  props An array of properties to use
+ * @return {Object}       The new object
+ */
+export default function pick (obj, props) {
+  // Create new object
+  const picked = {}
+
+  // Loop through props and push to new object
+  for (const prop of props) {
+    picked[prop] = obj[prop]
+  }
+
+  // Return new object
+  return picked
+}
