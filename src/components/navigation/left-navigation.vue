748847261cabd0d88ce58e1b1f6200b9df7296d3
Pugakn <ipugakn@gmail.com>
2022-03-25 19:38:16 -0600

    Fix ortganization button on smaller screens

--- src/components/navigation/left-navigation.vue
+++ src/components/navigation/left-navigation.vue
@@ -76,7 +76,7 @@ export default {
                 dho-btn(v-bind="dao" @click="switchDao(dao.name)")
         .col-auto.q-my-sm.q-px-sm
           .full-width.border-bot
-    .col-4.fixed-center
+    .col-4.fixed-center#nav-buttons
       .row.justify-center
         q-btn.q-ma-md(:flat="activeTab !== 'dashboard'" unelevated rounded padding="12px" icon="fas fa-home"  size="sm" :color="activeTab === 'dashboard' ? 'primary' : 'disabled'" :to="{ name: 'dashboard' }")
           q-tooltip(anchor="center right" self="center left" :content-style="{ 'font-size': '1em' }") Dashboard
@@ -93,6 +93,8 @@ export default {
 </template>
 
 <style lang="stylus" scoped>
+#nav-buttons
+  height auto
 .overTop
   z-index: 2
   overflow: 'auto'
