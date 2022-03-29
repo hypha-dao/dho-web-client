53be4f4316f175fde0b514e2fd9aaefdca0b938a
Pugakn <ipugakn@gmail.com>
2022-03-28 19:22:54 -0600

    Fix Profile creation redirection

--- src/mixins/profile-required.js
+++ src/mixins/profile-required.js
@@ -2,17 +2,40 @@ import { mapGetters } from 'vuex'
 
 export const profileRequired = {
   computed: {
-    ...mapGetters('accounts', ['account', 'isAuthenticated']),
+    ...mapGetters('accounts', ['account', 'isAuthenticated', 'isMember']),
     ...mapGetters('profiles', ['profiles'])
   },
 
-  updated () {
-    if (
-      this.isAuthenticated &&
-      this.$router.currentRoute.name !== 'profile-creation' &&
-      !this.profiles[this.account]
-    ) {
-      this.$router.push({ name: 'profile-creation', params: { username: this.account } })
+  methods: {
+    checkProfileRequiredRoute () {
+      if (
+        this.isMember &&
+        this.account &&
+        this.isAuthenticated &&
+        this.$router.currentRoute.name !== 'profile-creation' &&
+        !this.profiles[this.account]
+      ) {
+        this.$router.push({ name: 'profile-creation', params: { username: this.account } })
+      }
     }
+  },
+
+  watch: {
+    account: {
+      handler () {
+        this.checkProfileRequiredRoute()
+      },
+      immediate: false
+    },
+    isMember: {
+      handler () {
+        this.checkProfileRequiredRoute()
+      },
+      immediate: false
+    }
+  },
+
+  updated () {
+    this.checkProfileRequiredRoute()
   }
 }
