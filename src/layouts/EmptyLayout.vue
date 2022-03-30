d0d2ebfdb405d97c6862de5054794cc4ce53c953
Jose <jmgayosso@mightywizards.com>
2022-03-29 18:01:30 -0600

    Root page overrited and sticky position for explorer page

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'empty-layout'
+}
+</script>
+
+<template lang="pug">
+q-layout.bg-white(view="lHr LpR fFf" ref="layout")
+    q-page-container.window-height.q-py-md(:class="{ 'q-pr-md': $q.screen.gt.sm }")
+        q-scroll-area.full-height(:thumb-style=" { 'border-radius': '6px' }" ref="scrollArea")
+            .row.justify-center
+                .col-md-8.col-sm-10.col-xs-11
+                    q-card.card-container.bg-internal-bg.q-pa-md
+                        router-view
+</template>
+
+<style lang="sass" scoped>
+.card-container
+  min-height: 88vh
+  border-radius: 20px
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,25 +2,25 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/EmptyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/dashboard/dashboard.vue') },
-      { path: 'dashboard', component: () => import('pages/dashboard/dashboard.vue') },
-      { path: 'alert-manager', component: () => import('pages/alert-manager/alert-manager-form.vue') },
-      // { path: 'members/add', component: () => import('pages/members/add/members-add.vue') },
-      // { path: 'members/add/success', component: () => import('pages/members/add/success.vue') },
-      { path: 'members', component: () => import('pages/members/list/members-list.vue') },
-      { path: 'applicants', component: () => import('pages/applicants/list/applicants-list.vue') },
-      { path: 'old/@:username', component: () => import('pages/profiles/view/profile-view.vue') },
-      { path: '@:username', component: () => import('pages/profiles/Profile.vue'), props: true },
-      { path: '@:username/edit', component: () => import('pages/profiles/edit/profile-edit.vue') },
-      { path: 'old/wallet', component: () => import('pages/profiles/view/wallet-view.vue') },
-      { path: 'wallet', component: () => import('pages/profiles/Payments.vue') },
-      { path: 'roles', component: () => import('pages/roles/Apply.vue') },
-      { path: 'treasury', component: () => import('pages/treasury/treasury.vue') },
-      { path: 'multi-sig', component: () => import('pages/multi-sig/multi-sig-list.vue') },
-      { path: 'documents-proposal/:type/:user?', component: () => import('pages/documents-proposal/list.vue') },
-      { path: 'documents/:type/:user?', component: () => import('pages/documents/list.vue') }
+      { path: '', component: () => import('pages/dho/Explore.vue') }
+      // { path: 'dashboard', component: () => import('pages/dashboard/dashboard.vue') },
+      // { path: 'alert-manager', component: () => import('pages/alert-manager/alert-manager-form.vue') },
+      // // { path: 'members/add', component: () => import('pages/members/add/members-add.vue') },
+      // // { path: 'members/add/success', component: () => import('pages/members/add/success.vue') },
+      // { path: 'members', component: () => import('pages/members/list/members-list.vue') },
+      // { path: 'applicants', component: () => import('pages/applicants/list/applicants-list.vue') },
+      // { path: 'old/@:username', component: () => import('pages/profiles/view/profile-view.vue') },
+      // { path: '@:username', component: () => import('pages/profiles/Profile.vue'), props: true },
+      // { path: '@:username/edit', component: () => import('pages/profiles/edit/profile-edit.vue') },
+      // { path: 'old/wallet', component: () => import('pages/profiles/view/wallet-view.vue') },
+      // { path: 'wallet', component: () => import('pages/profiles/Payments.vue') },
+      // { path: 'roles', component: () => import('pages/roles/Apply.vue') },
+      // { path: 'treasury', component: () => import('pages/treasury/treasury.vue') },
+      // { path: 'multi-sig', component: () => import('pages/multi-sig/multi-sig-list.vue') },
+      // { path: 'documents-proposal/:type/:user?', component: () => import('pages/documents-proposal/list.vue') },
+      // { path: 'documents/:type/:user?', component: () => import('pages/documents/list.vue') }
     ]
   },
   { path: '/welcome', component: () => import('pages/onboarding/welcome.vue') },
