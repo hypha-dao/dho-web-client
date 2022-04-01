ec2b18d0dfe2c1763f175cff58e1a42d0124cdc9
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-01 09:09:26 -0600

    Add a title and subtitle for the empty layout

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/EmptyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/dho/Explore.vue') }
+      {
+        path: '/',
+        component: () => import('pages/dho/Explore.vue'),
+        meta: {
+          title: 'Explore all DAOs in the Hypha Universe',
+          subtitle: 'Find out more about how to set up your own DAO and Hypha here: https://hypha.earth'
+        }
+      }
       // { path: 'dashboard', component: () => import('pages/dashboard/dashboard.vue') },
       // { path: 'alert-manager', component: () => import('pages/alert-manager/alert-manager-form.vue') },
       // // { path: 'members/add', component: () => import('pages/members/add/members-add.vue') },
