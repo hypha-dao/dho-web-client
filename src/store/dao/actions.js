86faea71db5ac09a585c814b4187b47aaae637ea
Arsenije Savić <arsenijesavic@me.com>
2022-04-01 17:25:09 -0600

    Add create dao action

--- src/store/dao/actions.js
+++ src/store/dao/actions.js
@@ -1,5 +1,72 @@
 import camelToSnakeCase from '~/utils/camelToSnakeCase'
 
+export const createDAO = async function (context, { data }) {
+  const actions = [{
+    account: this.$config.contracts.dao,
+    name: 'createdao',
+    config: [
+      // GROUP: details
+      [
+        { label: 'content_group_label', value: ['string', 'details'] },
+        { label: 'dao_name', value: ['name', data?.name] },
+        { label: 'dao_title', value: ['string', data?.name] },
+        { label: 'dao_description', value: ['string', data?.description] },
+
+        { label: 'is_template', value: ['int64', 0] },
+        { label: 'dao_template', value: ['int64', data?.template] },
+
+        { label: 'reward_token', value: ['asset', `${data?.utilityDigits} ${data?.utilitySymbol}`] },
+        { label: 'reward_token_max_supply', value: ['assset', data?.utilityAmount] },
+        { label: 'reward_to_peg_ratio', value: ['assset', data?.utilityValue] },
+        { label: 'voice_token', value: ['asset', `${data?.voiceDigits} ${data?.voiceSymbol}`] },
+        { label: 'peg_token', value: ['asset', `${data?.treasuryDigits} ${data?.treasurySymbol}`] },
+        { label: 'use_seeds', value: ['int64', data?.use_seeds ? 1 : 0] },
+
+        { label: 'voting_duration_sec', value: ['int64', data?.votingDurationSec] },
+        { label: 'period_duration_sec', value: ['int64', data?.periodDurationSec] },
+        { label: 'voting_alignment_x100', value: ['int64', data?.votingAlignmentPercent] },
+        { label: 'voting_quorum_x100', value: ['int64', data?.votingQuorumPercent] },
+
+        //TODO
+        { label: 'voice_token_decay_period', value: ['int64', 0] },
+        { label: 'voice_token_decay_per_period_x10M', value: ['int64', 0] },
+
+
+        { label: 'reward_token_multiplier_x100', value: ['int64', data?.utilityTokenMultiplier] },
+        { label: 'voice_token_multiplier_x100', value: ['int64', data?.voiceTokenMultiplier] },
+        { label: 'peg_token_multiplier_x100', value: ['int64', data?.treasuryTokenMultiplier] },
+
+        { label: 'onboarder_account', value: ['name', data?.onboarder_account] }
+      ],
+
+      // GROUP: core members
+      [
+        { label: 'content_group_label', value: ['string', 'core_members'] },
+        // EXAMPLE: { label: 'account', value: ['name', data?.name] }
+        ...data?.members.map(member => ({ label: 'account', value: ['name', member?.account] }))
+      ],
+
+      // GROUP: salary bands
+      [
+        { label: 'content_group_label', value: ['string', 'salary_bands'] },
+        // EXAMPLE: { "label": "Band 1", "value": ["asset", "70000.00 USD"] },
+        ...data?.salaries.map(salary => ({ label: salary?.name, value: ['asset', `${parseFloat(salary?.value)}.00 USD`] }))
+      ],
+
+      // GROUP: salary style
+      [
+        { label: 'content_group_label', value: ['string', 'style'] },
+        { label: 'logo', value: ['string', data?.logo] }
+      ]
+    ]
+
+  }]
+
+  // console.log(JSON.stringify(actions))
+
+  return this.$api.signTransaction(actions)
+}
+
 export const updateSettings = async function (context, { docId, data }) {
   const actions = [{
     account: this.$config.contracts.dao,
