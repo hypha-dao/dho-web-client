e1570fc2c5c13061b72373a8729286b3c1485922
Pugakn <ipugakn@gmail.com>
2022-04-06 22:48:50 -0500

    Updated sidebar texts

--- src/components/navigation/sidebar-news.vue
+++ src/components/navigation/sidebar-news.vue
@@ -20,11 +20,9 @@ export default {
     .row.items-center.q-my-md
       .h-b2 Nice to see you here. Go take a look around. This DAO is here to help you govern your decentralized organisation, reduce coordination cost and build your organisation.
     .row.items-center.q-my-md
-      .h-b2 You can vote for <router-link :to="`/${daoName}/proposals`">proposals</router-link>, find other <router-link :to="`/${daoName}/members`">members</router-link> and find out what makes your <router-link :to="`/${daoName}/organization`">organization</router-link> what it is.
+      .h-b2 You can vote for <router-link :to="`/${daoName}/proposals`">proposals</router-link>, search for <router-link :to="`/${daoName}/members`">members</router-link> and find out what makes your <router-link :to="`/${daoName}/organization`">organization</router-link> tick.
     .row.items-center.q-my-md
-      .h-b2 Go to your <router-link :to="`/${daoName}/@${username}`">profile</router-link> and make sure its complete.
-    .row.items-center.q-my-md
-      .h-b2 And most importantly - have fun and do the DHO.
+      .h-b2 Be sure to complete your <router-link :to="`/${daoName}/@${username}`">profile</router-link> and have fun co-creating new and exciting things with your DAO!
 </template>
 
 <style lang="stylus" scoped>
