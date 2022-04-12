f660f5e2768f5bfe0458352f173fb3d1e2086da8
Arsenije Savić <arsenijesavic@me.com>
2022-04-12 12:14:07 -0500

    Add highlight of the active link to the qucik links

--- src/components/navigation/quick-links.vue
+++ src/components/navigation/quick-links.vue
@@ -6,15 +6,21 @@ export default {
 
   props: {
     compact: Boolean,
-    username: String,
-    isMember: Boolean
+    isMember: Boolean,
+    username: String
+  },
+
+  data: function () {
+    return {
+      activeRouteName: null
+    }
   },
 
   methods: {
     ...mapActions('accounts', ['logout']),
-    changeRoute (name, params) {
-      this.$router.push({ name: name, params })
-    }
+
+    changeRoute (name, params) { this.$router.push({ name: name, params }) },
+    isActiveRoute (name) { return this.activeRouteName === name }
   },
   computed: {
     ...mapGetters('dao', ['selectedDao']),
@@ -23,6 +29,10 @@ export default {
         '--button-size': this.compact ? '96px' : '140px'
       }
     }
+  },
+
+  watch: {
+    $route (to) { this.activeRouteName = to.name }
   }
 }
 </script>
@@ -31,20 +41,20 @@ export default {
 .quick-links.full-width(:style="cssVars")
   .row.q-gutter-xs.justify-center
     .col-6.button-square
-      q-btn.fit.items-end(:to="isMember ? { name: 'proposal-create', params: { dhoname: selectedDao.name } } : {}" rounded unelevated color="internal-bg" :disabled="!isMember")
+      q-btn.fit.items-end(:to="isMember ? { name: 'proposal-create', params: { dhoname: selectedDao.name } } : {}" rounded unelevated :color="isActiveRoute('proposal-create') ? 'primary' : 'internal-bg'" :text-color="isActiveRoute('proposal-create') ? 'internal-bg' : 'primary'" :disabled="!isMember")
         .column.items-center
-          q-icon.q-pa-xs(color="primary" size="md" name="fas fa-file-medical")
-          .text-caption.text-no-wrap.text-primary.text-bold New Proposal
+          q-icon.q-pa-xs(size="md" name="fas fa-file-medical")
+          .text-caption.text-no-wrap.text-bold New Proposal
     .col-6.button-square
-      q-btn.fit(@click="changeRoute('profile', {username})" rounded unelevated color="internal-bg" :disabled="!isMember")
+      q-btn.fit(@click="changeRoute('profile', {username})" rounded unelevated :color="isActiveRoute('profile') ? 'primary' : 'internal-bg'" :text-color="isActiveRoute('profile') ? 'internal-bg' : 'primary'" :disabled="!isMember")
         .column.items-center
-          q-icon.q-pa-xs(color="primary" size="md" name="far fa-user")
-          .text-caption.text-no-wrap.text-primary.text-bold My Profile
+          q-icon.q-pa-xs( size="md" name="far fa-user")
+          .text-caption.text-no-wrap.text-bold My Profile
     .col-6.button-square
-      q-btn.fit(@click="changeRoute('profile', {username})" rounded unelevated color="internal-bg" :disabled="!isMember")
+      q-btn.fit(@click="changeRoute('wallet')" rounded unelevated :color="isActiveRoute('wallet') ? 'primary' : 'internal-bg'" :text-color="isActiveRoute('wallet') ? 'internal-bg' : 'primary'" :disabled="!isMember")
         .column.items-center
-          q-icon.q-pa-xs(color="primary" size="md" name="fas fa-wallet")
-          .text-caption.text-no-wrap.text-primary.text-bold My Wallet
+          q-icon.q-pa-xs(size="md" name="fas fa-wallet")
+          .text-caption.text-no-wrap.text-bold My Wallet
     .col-6.button-square
       q-btn.fit(@click="logout" rounded unelevated color="internal-bg")
         .column.items-center
