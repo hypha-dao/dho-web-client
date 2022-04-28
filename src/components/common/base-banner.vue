8a01b6b7cd6c387452ad7d44ae1fc0103dfc85dd
Pugakn <ipugakn@gmail.com>
2022-04-28 15:44:19 -0500

    Added banner patterns
    + Added colorToFilter util

--- src/components/common/base-banner.vue
+++ src/components/common/base-banner.vue
@@ -1,4 +1,16 @@
 <script>
+import { hexToFilter } from '~/utils/colorToFilter.js'
+import { colors } from 'quasar'
+const { getPaletteColor } = colors
+
+const patternImageMap = {
+  geometric1: 'geometric1.svg',
+  geometric2: 'geometric2.svg',
+  geometric3: 'geometric3.svg',
+  organic1: 'organic1.svg',
+  organic2: 'organic2.svg',
+  organic3: 'organic3.svg'
+}
 export default {
   name: 'base-banner',
   components: {
@@ -7,12 +19,31 @@ export default {
   props: {
     title: String,
     description: String,
-    background: String
+    background: String,
+    pattern: {
+      type: String, // geometric1, geometric2, geometric3, organic1, organic2, organic3
+      default: undefined
+    },
+    patternColor: {
+      type: String,
+      default: getPaletteColor('secondary')
+    },
+    patternAlpha: {
+      type: Number,
+      default: 0.3
+    }
   },
   computed: {
     backgroundStyle () {
       return {
         backgroundImage: `url('${require('../../assets/images/' + this.background)}')`
+      }
+    },
+    patternStyle () {
+      return {
+        backgroundImage: `url('${require('../../assets/images/banner-patterns/' + patternImageMap[this.pattern])}')`,
+        filter: hexToFilter(this.patternColor),
+        opacity: this.patternAlpha
       }
     }
   }
@@ -20,32 +51,55 @@ export default {
 </script>
 
 <template lang="pug">
-.base-banner.full-width(:style="backgroundStyle")
-  q-btn.absolute-top-right.q-mt-md.q-mr-md.q-pa-xs.close-btn(
-    flat round size="sm"
-    icon="fas fa-times"
-    color="white"
-    @click="$emit('onClose')"
-  )
-  .row.q-py-xxxl.q-px-xxl.full-height
-    .col-6
-      .column.justify-between.flex.full-height
-        q-markdown(:src="title" content-class="h-h2 text-white")
-        .h-b1.text-white.text-description.q-mb-md {{description}}
-        .row.q-gutter-sm
-          slot(name="buttons")
-    .col-6
-       slot(name="right")
+.base-banner.full-width.rounded-corners.relative
+  #banner-image.rounded-corners(:style="backgroundStyle" v-if="background").absolute
+  #banner-pattern.rounded-corners(:style="patternStyle" v-if="pattern").absolute
+  #linear-gradient.rounded-corners.absolute
+  .content
+    q-btn.absolute-top-right.q-mt-md.q-mr-md.q-pa-xs.close-btn(
+      flat round size="sm"
+      icon="fas fa-times"
+      color="white"
+      @click="$emit('onClose')"
+    )
+    .row.q-py-xxxl.q-px-xxl.full-height
+      .col-6
+        .column.justify-between.flex.full-height
+          q-markdown(:src="title" content-class="h-h2 text-white")
+          .h-b1.text-white.text-description.q-mb-md {{description}}
+          .row.q-gutter-sm
+            slot(name="buttons")
+      .col-6
+        slot(name="right")
 </template>
 
 <style lang="stylus" scoped>
-.base-banner
-  border-radius 32px
-  min-height 300px
+
+#linear-gradient
+  width 100%
+  height 100%
+  background: linear-gradient(268deg, rgba(19,34,106, 0) 0%, rgba(24,30,78, 0.9) 100%)
+
+#banner-pattern, #banner-image
+  z-index 0
+  width: 100%
+  height: 100%
   background-repeat: no-repeat
   background-size: cover
-  background-color: $primary
   background-position-x right
+
+#banner-image
+  z-index 1
+
+.content >>> div
+  z-index 1
+
+.rounded-corners
+  border-radius 32px
+
+.base-banner
+  min-height 300px
+  background-color: $primary
 
   .text-description
     font-weight 300
--- src/pages/dho/Members.vue
+++ src/pages/dho/Members.vue
@@ -371,22 +371,22 @@ export default {
         q-btn(class="h7" color="white" no-caps flat rounded label="Copy invite link" @click="copyToClipBoard")
           q-tooltip Send a link to your friends to invite them to join this DAO
 
-    .row.full-width.q-py-md
-      .col-9
-        members-list(:members="members" :view="view" @loadMore="onLoadMoreMembers" ref="scroll")
-      .col-3.q-pl-sm
-        filter-widget.sticky(:view.sync="view",
-        :toggle.sync="showApplicants",
-        :sort.sync="sort",
-        :textFilter.sync="textFilter",
-        :circle.sync="circle",
-        :optionArray.sync="optionArray",
-        :circleArray.sync="circleArray"
-        :viewSelectorLabel="'Members view'",
-        :showToggle="true",
-        :showCircle="false"
-        :toggleLabel="'Show applicants'"
-        filterTitle="Filter by account name"
+  .row.full-width.q-py-md
+    .col-9
+      members-list(:members="members" :view="view" @loadMore="onLoadMoreMembers" ref="scroll")
+    .col-3.q-pl-sm
+      filter-widget.sticky(:view.sync="view",
+      :toggle.sync="showApplicants",
+      :sort.sync="sort",
+      :textFilter.sync="textFilter",
+      :circle.sync="circle",
+      :optionArray.sync="optionArray",
+      :circleArray.sync="circleArray"
+      :viewSelectorLabel="'Members view'",
+      :showToggle="true",
+      :showCircle="false"
+      :toggleLabel="'Show applicants'"
+      filterTitle="Filter by account name"
       )
 </template>
 
