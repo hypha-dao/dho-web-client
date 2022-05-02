bc30407f98eb68a325ce1f4e52c07c10d237451a
Arsenije Savic <arsenijesavic@me.com>
2022-05-02 13:03:56 +0200

    Remove bio from the member profile-card

--- src/components/profiles/profile-card.vue
+++ src/components/profiles/profile-card.vue
@@ -256,9 +256,9 @@ widget-editable(
             .text-grey-7.text-no-wrap.h-b2 {{ voiceToken.token }}
     .col-6(:class="{ 'col-12': card, 'col-7': isEnroller, 'q-px-xs': card }" v-if="isApplicant")
       .row.items-center.flex.justify-center.full-height
-        .col-8(:class="{ 'text-center': card, 'col-12': !isEnroller || card }")
-          .items-center(:class="{ 'row': list, 'column': card }")
-            .text-grey-7.body2.applicant-description(v-if="!isEnroller || list") {{publicData.bio && (publicData.bio.substr(0, card ? 90 : 200) + (publicData.bio.length > (card ? 90 : 200) ? "..." : ""))}}
+        //- .col-8(:class="{ 'text-center': card, 'col-12': !isEnroller || card }")
+        //-   .items-center(:class="{ 'row': list, 'column': card }")
+        //-     .text-grey-7.body2.applicant-description(v-if="!isEnroller || list") {{publicData.bio && (publicData.bio.substr(0, card ? 90 : 200) + (publicData.bio.length > (card ? 90 : 200) ? "..." : ""))}}
         .col-4(:class="{ 'text-center': card , 'col-12': card, 'q-mt-md': card}" v-if= "isEnroller")
           q-btn.full-width(
           color="primary"
