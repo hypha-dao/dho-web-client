5a4abc4a345520084a73c58e95ebe4bc51146a9c
DaniPoot <danielpoot.uc2@gmail.com>
2022-05-18 13:20:36 -0500

    Add new stories for voting

--- src/components/proposals/voting.stories.js
+++ src/components/proposals/voting.stories.js
@@ -39,9 +39,89 @@ const Template = (args, { argTypes }) => ({
   })
 })
 
-export const Example = Template.bind({})
-Example.args = {
-
+export const Gest = Template.bind({})
+Gest.args = {
+  unity: 0.85,
+  quorum: 0.15,
+  expiration: '20/May/2122',
+  stagin: false,
+  vote: null,
+  status: 'proposed'
 }
 
 export const Base = Template.bind({})
+Base.args = {
+  ...Gest.args,
+  activeButtons: true
+}
+
+export const BaseVotePass = Template.bind({})
+BaseVotePass.args = {
+  ...Gest.args,
+  activeButtons: true,
+  vote: 'pass'
+}
+
+export const BaseVoteAbstain = Template.bind({})
+BaseVoteAbstain.args = {
+  ...Gest.args,
+  activeButtons: true,
+  vote: 'abstain'
+}
+
+export const BaseVoteFail = Template.bind({})
+BaseVoteFail.args = {
+  ...Gest.args,
+  activeButtons: true,
+  vote: 'fail'
+}
+
+export const Approved = Template.bind({})
+Approved.args = {
+  ...Gest.args,
+  activeButtons: true,
+  status: 'approved',
+  expiration: '8/May/2022'
+}
+
+export const Apply = Template.bind({})
+Apply.args = {
+  ...Gest.args,
+  activeButtons: true,
+  status: 'approved',
+  unity: 0.50,
+  quorum: 0.85,
+  type: 'Badge',
+  expiration: '8/May/2022'
+}
+
+export const Withdraw = Template.bind({})
+Withdraw.args = {
+  ...Gest.args,
+  status: 'approved',
+  active: true,
+  type: 'Badge',
+  expiration: '8/May/2022'
+}
+
+export const Suspended = Template.bind({})
+Suspended.args = {
+  ...Gest.args,
+  activeButtons: true,
+  status: 'suspended',
+  expiration: '8/May/2022'
+}
+
+export const Archived = Template.bind({})
+Archived.args = {
+  ...Gest.args,
+  status: 'rejected',
+  expiration: '8/May/2022'
+}
+
+export const Withdrawed = Template.bind({})
+Withdrawed.args = {
+  ...Gest.args,
+  status: 'withdrawed',
+  expiration: '8/May/2022'
+}
