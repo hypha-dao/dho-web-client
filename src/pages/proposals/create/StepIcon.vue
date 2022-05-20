1f9b03dd6152ea8da1604add29d7424b44a4867d
Arsenije Savic <arsenijesavic@me.com>
2022-05-20 13:10:55 +0200

    Remove commented code

--- src/pages/proposals/create/StepIcon.vue
+++ src/pages/proposals/create/StepIcon.vue
@@ -170,25 +170,6 @@ widget
       )
         template(v-slot:prepend)
           q-icon(name="fas fa-search" size="xs" color="primary")
-        //- q-btn.q-px-xl.q-ma-md(
-        //-   dense
-        //-   no-caps
-        //-   rounded
-        //-   outline
-        //-   color="primary"
-        //-   label="Upload an image"
-        //-   @click="onChoosingImage"
-        //-   v-show="!choosingImage"
-        //- )
-        //- input-file-ipfs.q-mr-lg.q-ml-sm.q-mb-md(
-        //-   :ipfsURL="selectedImage"
-        //-   preview
-        //-   image
-        //-   @uploadedFile="onImageSelected"
-        //-   previewSize="90px"
-        //-   v-show="choosingImage"
-        //-   ref="ipfsInput"
-        //- )
     div( style="max-height: 500px; overflow: auto;" ref="scrollTargetRef")
       q-infinite-scroll(@load="onLoad" :offset="250" :scroll-target="$refs.scrollTargetRef")
         template(v-slot:loading)
@@ -203,15 +184,6 @@ widget
               :background="selectedIcon === `${icon[1].type || 'far'}  fa-${icon[0]}` ? 'bg-primary' : 'bg-grey-3'"
               :color="selectedIcon === `${icon[1].type || 'far'}  fa-${icon[0]}` ? 'white' : 'primary'"
             )
-    //- .row.full-width.q-col-gutter-sm(v-if="filteredIcons")
-    //-   .col-sm-2(v-for="icon in filteredIcons" :key="icon[0]")
-    //-     icon-card.full-width(
-    //-         :name="`${icon[1].type || 'far'}  fa-${icon[0]}`"
-    //-         size="lg"
-    //-         @click="onIconSelected"
-    //-         :background="selectedIcon === `${icon[1].type || 'far'}  fa-${icon[0]}` ? 'bg-primary' : 'bg-grey-3'"
-    //-         :color="selectedIcon === `${icon[1].type || 'far'}  fa-${icon[0]}` ? 'white' : 'primary'"
-    //-     )
   nav.row.justify-end.q-mt-xl.q-gutter-xs
     q-btn.q-px-xl(
       @click="$emit('prev')"
