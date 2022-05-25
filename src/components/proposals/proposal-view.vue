67ee6f5b380a689a5e9099f07fa84f090639d68a
DaniPoot <danielpoot.uc2@gmail.com>
2022-05-24 19:08:57 -0500

    Change the validation

--- src/components/proposals/proposal-view.vue
+++ src/components/proposals/proposal-view.vue
@@ -268,7 +268,7 @@ widget.proposal-view.q-mb-sm
   .row.q-my-sm(v-if="tokens")
     .col.bg-internal-bg.rounded-border
       .row.q-ml-md.q-py-md.text-bold(v-if="withToggle" ) {{ compensationLabel }}
-      payout-amounts(:daoLogo="daoSettings.logo" :tokens="toggle ? tokens : tokensByCycle" :class="{ 'q-pa-md': !withToggle }")
+      payout-amounts(:daoLogo="daoSettings.logo" :tokens="!toggle ? tokens : tokensByCycle" :class="{ 'q-pa-md': !withToggle }")
       .row.items-center.q-py-md.q-ml-xxs(v-if="withToggle")
         .col-1
           q-toggle(v-model="toggle" size="md")
