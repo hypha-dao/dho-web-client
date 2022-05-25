910b9857f2c9df1f42f4030e59cd57dc8d94cdeb
Pugakn <ipugakn@gmail.com>
2022-05-24 20:48:16 -0500

    Updated documentation link

--- src/components/dashboard/support-widget.vue
+++ src/components/dashboard/support-widget.vue
@@ -13,7 +13,7 @@ widget(title="Need support?")
     .h-b2.q-mt-md Please read our Documentation for more info. If you are stuck with a problem you can also reach out to us on discord in the "dho-support" channel.
     .row.justify-between.flex.items-center.q-mb-sm
       .col-auto
-        q-btn.q-mt-md.q-px-sm.text-white(noCaps rounded color="primary" type="a" href="https://notepad.hypha.earth/5dC66nNXRVGpb1aTHaRJXw" target="_blank") Documentation
+        q-btn.q-mt-md.q-px-sm.text-white(noCaps rounded color="primary" type="a" href="https://notepad.hypha.earth/DBKEBKmIRMqqn9heCRf21w" target="_blank") Documentation
       .col
         q-btn.q-mt-md.q-ml-sm.discord-buttom(unelevated rounded color="internal-bg" icon="fab fa-discord" size="0.7rem" type="a" href="https://discord.com/channels/722537361480613950/732285564384051323" target="_blank")
 </template>
