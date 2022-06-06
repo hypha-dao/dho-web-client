980096af05f818d445ba54c882ebda73a78b6e07
Arsenije Savic <arsenijesavic@me.com>
2022-06-06 18:14:22 +0200

    Update load alert

--- src/store/layout/actions.js
+++ src/store/layout/actions.js
@@ -1,9 +1,8 @@
 export const loadAlert = async function ({ commit }) {
   const response = await this.$apollo.query({
-    query: require('~/query/dho-alerts.gql')
+    query: require('~/query/dao-alerts.gql')
   })
-  const level = response.data.queryDho[0].alert[0]?.details_level_n
-  const content = response.data.queryDho[0].alert[0]?.details_content_s
+  const { level, content } = response.data.queryAlert[0]
   if (level && content) {
     commit('setAlert', { level, content })
     return { level, content }
