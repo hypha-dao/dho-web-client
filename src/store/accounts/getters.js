7b33cdc89e4daf193cc410c714b6d2db162e920a
Arsenije Savic <arsenijesavic@me.com>
2022-06-06 11:05:35 +0200

    Add isProduction getter

--- src/store/accounts/getters.js
+++ src/store/accounts/getters.js
@@ -1,3 +1,4 @@
+
 export const account = ({ account }) => account
 export const isAdmin = ({ admin }) => admin
 export const isApplicant = ({ applicant }) => applicant
@@ -5,4 +6,5 @@ export const isAuthenticated = ({ account }) => account !== null
 export const isEnroller = ({ enroller }) => enroller
 export const isHyphaOwner = ({ isHyphaOwner }) => isHyphaOwner
 export const isMember = ({ membership }) => membership
+export const isProduction = () => process.env.NODE_ENV === 'production'
 export const loading = ({ loading }) => loading
