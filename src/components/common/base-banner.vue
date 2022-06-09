20424511854eb825f79b6e0376d4f7f79ef019d1
Arsenije Savic <arsenijesavic@me.com>
2022-06-09 09:09:10 +0200

    Remove additional pattern props

--- src/components/common/base-banner.vue
+++ src/components/common/base-banner.vue
@@ -1,5 +1,4 @@
 <script>
-// import { hexToFilter } from '~/utils/colorToFilter.js'
 import { colors } from 'quasar'
 const { getPaletteColor } = colors
 
@@ -40,44 +39,15 @@ export default {
     pattern: {
       type: String,
       default: undefined
-    },
-    /**
-     * The pattern color
-     */
-    patternColor: {
-      type: String,
-      default: getPaletteColor('secondary')
-    },
-    /**
-     * The pattern opacity
-     */
-    patternAlpha: {
-      type: Number,
-      default: 0.3
-    }
-  },
-  computed: {
-    backgroundStyle () {
-      return {
-        backgroundImage: `url('${this.background}')`
-      }
-    },
-    patternStyle () {
-      return {
-        backgroundImage: `url('/patterns/${this.pattern}.svg')`,
-        // filter: hexToFilter(this.patternColor),
-        opacity: this.patternAlpha / 100
-      }
     }
   }
-
 }
 </script>
 
 <template lang="pug">
 .base-banner.full-width.rounded-corners.relative-position.overflow-hidden(:style="{'background': color}")
-  #banner-image.absolute(:style="backgroundStyle" v-if="background")
-  #banner-pattern.absolute(:style="patternStyle" v-if="!background && pattern")
+  #banner-image.absolute(:style="{'background': `url('${background}')`}" v-if="background")
+  #banner-pattern.absolute(:style="{'background': `url('${pattern}') repeat`, 'background-size': '200px' }" v-if="pattern")
   #linear-gradient.absolute.z-40
   .content.relative-position.z-50.full-height
     q-btn.absolute-top-right.q-mt-md.q-mr-md.q-pa-xs.close-btn(
