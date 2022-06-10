3230e5baf2f27fcc9512444fa42bb079d41df71d
Arsenije Savic <arsenijesavic@me.com>
2022-06-10 11:29:44 +0200

    Add webpack optimization

--- webpack-config.js
+++ webpack-config.js
@@ -12,6 +12,32 @@ module.exports = (cfg) => {
     patterns: [{ from: './public/*.json', to: './', force: true, flatten: true }]
   }))
 
+  cfg.optimization = {
+    minimize: true,
+    nodeEnv: 'production',
+
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+
   cfg.module.rules.push({
     enforce: 'pre',
     test: /\.(js|vue)$/,
