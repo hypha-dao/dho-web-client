8540f60a4abeec9e82e1fbfe987dc7ffb74903a4
Arsenije Savic <arsenijesavic@me.com>
2022-06-13 13:58:39 +0200

    Create comments input component

---
+++ src/components/proposals/comment-input.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+  name: 'comments-input',
+
+  data () {
+    return {
+      comment: ''
+    }
+  },
+
+  methods: {
+    createComment () {
+      if (this.comment.trim() === '') return
+      this.$emit('create', { content: this.comment })
+      this.comment = ''
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+.comments-input
+    q-input.q-my-md.rounded-border(
+        :debounce="200"
+        @keyup.enter="createComment"
+        bg-color="white"
+        color="primary"
+        dense
+        lazy-rules
+        outlined
+        placeholder="Type a comment here..."
+        ref="name"
+        rounded
+        v-model="comment"
+    )
+</template>
