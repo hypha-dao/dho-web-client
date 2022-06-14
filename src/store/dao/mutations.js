b4e409bb4bb84ae687aba703992af3b0864ae71f
Pugakn <ipugakn@gmail.com>
2022-06-14 15:45:49 -0500

    Cash disabled only for hypha

--- src/store/dao/mutations.js
+++ src/store/dao/mutations.js
@@ -70,7 +70,7 @@ export const switchDao = (state, daos) => {
       url: settings.settings_daoUrl_s,
 
       registrationEnabled: !settings.settings_isHypha_i, // Currently disabled for hypha, TODO: obtain flag from server
-      cashClaimsEnabled: false // TODO: Flag from server?
+      cashClaimsEnabled: !settings.settings_isHypha_i // TODO: Flag from server?
     }
   }
 }
