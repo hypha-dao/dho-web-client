5c5f9499b350fedc1be6c6002b4baf5e8ffdaa60
Pugakn <ipugakn@gmail.com>
2022-09-28 20:46:59 -0500

    feat(mobile): add asset filter

--- src/components/profiles/members-list.vue
+++ src/components/profiles/members-list.vue
@@ -37,7 +37,7 @@ export default {
 
 <template lang="pug">
 .members-list(ref="scrollContainer")
-  q-infinite-scroll(@load="onLoad" :offset="compact ? 0 : 250"  ref="scroll" :scroll-target="scrollContainer")
+  q-infinite-scroll(@load="onLoad" :offset="compact ? 0 : 250"  ref="scroll")
     .row.q-gutter-md(:class="{ 'full-width': view === 'list', 'q-pr-xxs': view === 'list', 'flex justify-center': $q.screen.lt.md }")
       template(v-for="member in members")
         profile-card(:username="member.username" :joinedDate="member.joinedDate" :isApplicant = "member.isApplicant" :view="view" :key="member.hash" :fullWidth="compact")
--- src/pages/proposals/ProposalList.vue
+++ src/pages/proposals/ProposalList.vue
@@ -477,7 +477,7 @@ export default {
         icon= "fas fa-file-medical" :actionButtons="[{label: 'Create a new Proposal', color: 'primary', onClick: () => $router.push(`/${this.daoSettings.url}/proposals/create`), disable: !isMember, disableTooltip: 'You must be a member'}]" )
       .q-mb-xl(v-show="showStagedProposals && filteredStagedProposals.length > 0")
         proposal-list(:username="account" :proposals="filteredStagedProposals" :supply="supply" view="card" compact)
-      q-infinite-scroll(@load="onLoad" :offset="500" ref="scroll" :initial-index="1" v-if="filteredProposals.length").scroll
+      q-infinite-scroll(@load="onLoad" :offset="0" ref="scroll" :initial-index="1" v-if="filteredProposals.length").scroll
         proposal-list(:username="account" :proposals="filteredProposals" :supply="supply" view="card" compact)
 </template>
 
