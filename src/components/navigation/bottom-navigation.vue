9a71a8507bef9480bdc2202661780172978cd4fe
Irving Puga <ipugakn@gmail.com>
2022-10-12 07:29:30 -0500

    fix(mobile): search columns (#1681)
    * fix(mobile): contact info columns
    
    * fix(mobile): bottom menu hover
    
    * fix(mobile): search filter

--- src/components/navigation/bottom-navigation.vue
+++ src/components/navigation/bottom-navigation.vue
@@ -22,13 +22,15 @@ export default {
 .bottom-navigation.full-width.full-height
   .row.justify-evenly
     q-btn.q-my-md(:flat="!isActive('dashboard')" unelevated rounded padding="12px" icon="fas fa-home"  size="sm" :color="isActive('dashboard') ? 'primary' : 'disabled'" :to="{ name: 'dashboard' }")
-      q-tooltip(anchor="top middle" self="bottom middle" :content-style="{ 'font-size': '1em' }") Dashboard
     q-btn.q-my-md(:flat="!isActive('proposals')" unelevated rounded padding="12px" icon="far fa-file-alt"  size="sm" :color="isActive('proposals') ? 'primary' : 'disabled'" :to="{ name: 'proposals' }")
-      q-tooltip(anchor="top middle" self="bottom middle" :content-style="{ 'font-size': '1em' }") Proposals
     q-btn.q-my-md(:flat="!isActive('members')" unelevated rounded padding="12px" icon="fas fa-users"  size="sm" :color="isActive('members') ? 'primary' : 'disabled'"  :to="{ name: 'members' }")
-      q-tooltip(anchor="top middle" self="bottom middle" :content-style="{ 'font-size': '1em' }") Members
     q-btn.q-my-md(:flat="!isActive('organization')" unelevated rounded padding="12px" icon="fas fa-building"  size="sm" :color="isActive('organization') ? 'primary' : 'disabled'"  :to="{ name: 'organization' }")
-      q-tooltip(anchor="top middle" self="bottom middle" :content-style="{ 'font-size': '1em' }") Organization
     q-btn.q-my-md(:flat="!isActive('explore')" unelevated rounded padding="12px" icon="fas fa-globe" size="sm" :color="isActive('explore') ? 'primary' : 'disabled'"  :to="{ name: 'explore' }")
-      q-tooltip(anchor="top middle" self="bottom middle" :content-style="{ 'font-size': '1em' }") Explore
 </template>
+
+<style lang="stylus" scoped>
+a:hover{
+  color: white !important;
+}
+
+</style>
