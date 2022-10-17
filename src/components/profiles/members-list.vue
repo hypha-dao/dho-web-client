7ae2de0304462061bc0c047c3117bb585935c761
Evgeni B <glum_psyche@gofree.dev>
2022-10-17 17:08:05 +0300

    feat(tablet): added tablet design for members page (portrait)

--- src/components/profiles/members-list.vue
+++ src/components/profiles/members-list.vue
@@ -38,8 +38,8 @@ export default {
 <template lang="pug">
 .members-list(ref="scrollContainer")
   q-infinite-scroll(@load="onLoad" :offset="compact ? 0 : 250"  ref="scroll")
-    .row.q-gutter-md(:class="{ 'full-width': view === 'list', 'q-pr-xxs': view === 'list', 'flex justify-center': $q.screen.lt.md }")
-      template(v-for="member in members")
+    .row(:class="{ 'full-width': view === 'list', 'q-pr-xxs': view === 'list', 'flex justify-center': $q.screen.lt.md, 'q-gutter-md':!$q.screen.sm }")
+      .template(v-for="member in members" :class="{ 'col-6 q-px-xs q-mb-md':$q.screen.sm }")
         profile-card(:username="member.username" :joinedDate="member.joinedDate" :isApplicant = "member.isApplicant" :view="view" :key="member.hash" :compact="compact")
     template(v-slot:loading)
       .row.justify-center.q-my-md
