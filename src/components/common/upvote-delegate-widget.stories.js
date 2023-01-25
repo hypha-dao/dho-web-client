72e610e2c839315f29bdb5e776710832be222808
Evgeni B <glum_psyche@gofree.dev>
2023-01-25 11:01:04 +0300

    feat(upvote-delegate-widget): update component story file (#1943)

--- src/components/common/upvote-delegate-widget.stories.js
+++ src/components/common/upvote-delegate-widget.stories.js
@@ -17,7 +17,22 @@ const Template = (args, { argTypes }) => ({
 export const Example = Template.bind({})
 // These are the params for the example story. You can all the params you want.
 Example.args = {
-  endDate: '2023-09-01'
+  endDate: '2023-09-01',
+  users: [
+    {
+      headDelegate: true,
+      name: 'User'
+    },
+    {
+      name: 'User'
+    },
+    {
+      name: 'User'
+    },
+    {
+      name: 'User'
+    }
+  ]
 }
 
 export const Base = Template.bind({})
