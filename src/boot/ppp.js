05acc9a85b1587b3dca23f1a47b495eae0f12441
Alex-IS <djokic.sasha@gmail.com>
2023-03-02 11:57:49 +0700

    fix: add BASE_APP as app id

--- src/boot/ppp.js
+++ src/boot/ppp.js
@@ -1,6 +1,6 @@
 import PPP from '@hypha-dao/ppp-client-api'
 
 export default async ({ store }) => {
-  PPP.configure(process.env.PPP_ENV)
+  PPP.configure(process.env.PPP_ENV, 'BASE_APP')
   store.$ppp = PPP
 }
