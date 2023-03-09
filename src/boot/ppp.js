756f1a45a982fa5756db674f2848b9c8e149f2df
Alex <djokic.sasha@gmail.com>
2023-03-09 23:24:58 +0700

    fix: remove BASE_APP when configuring ppp-client-api (#2068)

--- src/boot/ppp.js
+++ src/boot/ppp.js
@@ -1,6 +1,6 @@
 import PPP from '@hypha-dao/ppp-client-api'
 
 export default async ({ store }) => {
-  PPP.configure(process.env.PPP_ENV, 'BASE_APP')
+  PPP.configure(process.env.PPP_ENV)
   store.$ppp = PPP
 }
