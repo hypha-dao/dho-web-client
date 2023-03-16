5c29d23f5afc57527fe3d797940954d789082c7d
Arsenije Savić <arsenijesavic@me.com>
2023-03-15 20:44:49 -0600

    fix(home): add default grid (#2084)

--- src/pages/dho/Home.vue
+++ src/pages/dho/Home.vue
@@ -383,6 +383,10 @@ q-page.page-dashboard
   grid-row-gap: $space-md.y;
   min-height: 0;
   min-width: 0;
+  grid-template-columns: repeat(4, 1fr) 2fr;
+  grid-template-rows: minmax(0, 1fr);
+  grid-template-areas "assignments badges members proposals new new"\
+                      "support support how how new new"
 
   @media (max-width: $breakpoint-lg-max)
     grid-template-columns: repeat(4, 1fr) 2fr;
