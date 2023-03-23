6d26b14f7a29fa42b063dfddb665c0ca7f5cdaf9
Evgeni B <glum_psyche@gofree.dev>
2023-03-23 16:46:20 +0300

    feat(explore-page): added ecosystem filter widget in explore page (#1557)

--- src/pages/dho/Explore.vue
+++ src/pages/dho/Explore.vue
@@ -1,6 +1,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import ipfsy from '~/utils/ipfsy'
+import { dateToStringShort } from '~/utils/TimeUtils'
 
 export default {
   name: 'page-explore',
@@ -9,7 +10,8 @@ export default {
     DhoCard: () => import('~/components/navigation/dho-card.vue'),
     FilterOpenButton: () => import('~/components/filters/filter-open-button.vue'),
     FilterWidget: () => import('~/components/filters/filter-widget.vue'),
-    FilterWidgetMobile: () => import('~/components/filters/filter-widget-mobile.vue')
+    FilterWidgetMobile: () => import('~/components/filters/filter-widget-mobile.vue'),
+    Widget: () => import('~/components/common/widget.vue')
   },
 
   data () {
@@ -37,7 +39,8 @@ export default {
         width: this.$q.screen.md ? '400px' : '100%',
         right: this.$q.screen.md ? '0' : '0',
         left: this.$q.screen.md ? 'auto' : '0'
-      }
+      },
+      displayingItemsType: 'DAOS'
     }
   },
 
@@ -71,6 +74,22 @@ export default {
         }
       }
 
+    },
+    ecosystemsList: {
+      query () {
+        return require('~/query/ecosystem/ecosystems-list.gql')
+      },
+      update: data => {
+        return data?.queryDao?.map(ecosystem => {
+          return {
+            name: ecosystem.settings[0].ecosystem_name_s,
+            purpose: ecosystem.settings[0].ecosystem_purpose_s,
+            logo: ecosystem.settings[0].ecosystem_logo_s,
+            domain: ecosystem.settings[0].ecosystem_domain_s,
+            createdDate: ecosystem.createdDate
+          }
+        })
+      }
     }
   },
 
@@ -148,6 +167,8 @@ export default {
       }
     },
 
+    ipfsy,
+
     async resetPagination () {
       await this.$nextTick()
       this.$refs.scroll.stop()
@@ -155,6 +176,21 @@ export default {
       this.$refs.scroll.resume()
       await this.$nextTick()
       this.$refs.scroll.trigger()
+    },
+    formatDate (date) {
+      return dateToStringShort(date)
+    },
+    parseDomain (domain) {
+      switch (domain) {
+        case 'SOCIO_ECOLOGICAL':
+          return 'Socio-Ecological'
+        case 'SOCIO_ECONOMICAL':
+          return 'Socio-Economical'
+        case 'SOCIO_POLITICAL':
+          return 'Socio-Political'
+        case 'SOCIO_PSYCHOLOGICAL':
+          return 'Socio-Psychological'
+      }
     }
   },
 
@@ -186,13 +222,52 @@ q-page.page-explore
           q-btn.q-px-lg.h-btn1(no-caps rounded unelevated color="secondary" href="https://hypha.earth/" target="_blank") Discover More
 
   .row.q-py-md
-    .col-sm-12.col-md-12.col-lg-9(ref="scrollContainer")
+    .col-sm-12.col-md-12.col-lg-9(ref="scrollContainer" v-if="displayingItemsType === 'DAOS'")
       q-infinite-scroll(@load="onLoad" :offset="250" :scroll-target="$refs.scrollContainer" ref="scroll")
         .row
           .col-4.q-mb-md(v-for="(dho,index) in dhos" :key="dho.name" :class="{ 'col-6': $q.screen.lt.lg, 'q-pr-md': $q.screen.lt.sm ? false : $q.screen.gt.md ? true : index % 2 === 0, 'full-width':  view === 'list' || $q.screen.lt.sm}")
             dho-card.full-width(v-bind="dho" :view="view")
-
+    .col-9(v-if="displayingItemsType === 'ECOSYSTEMS'")
+      q-infinite-scroll(@load="onLoad" :offset="250" :scroll-target="$refs.scrollContainer" ref="scroll")
+        .row.q-col-gutter-md.q-mr-md
+          .full-width(v-for="(ecosystem,index) in ecosystemsList" :key="ecosystem.name")
+            widget.full-width
+              .row.items-center
+                .col-9.q-pr-xl.row.items-center
+                  .col-auto
+                    q-avatar(size="160px" color='primary')
+                      img(:src="ipfsy(ecosystem.logo)" v-if="ecosystem.logo").object-cover
+                      //- span(v-if="!ecosystem.logo") {{ecosystem.name}}
+                  .col.q-ml-md
+                    .row.items-center.q-mb-md
+                      .h-h4 {{ ecosystem.name }}
+                      .row.q-ml-md
+                        q-icon.q-py-xs(v-if="ecosystem.domain === 'SOCIO_ECOLOGICAL'" name="fas fa-leaf")
+                        .text-xs.q-ml-xs.q-mt-xs {{ parseDomain(ecosystem.domain) }}
+                    .text-xs.text-h-gray {{ ecosystem.purpose }}
+                q-separator(:vertical="true" color="grey-3" inset)
+                .col
+                  .column.justify-between.full-width.full-height.items-center
+                    div
+                      .row.items-center
+                        .h-h4.q-mb-sm 4 DAOs
+                      .row.items-center
+                        q-icon.q-py-xs(color="primary" name="fas fa-calendar-alt")
+                        .text-xs.text-h-gray.q-px-xs {{ formatDate(ecosystem.createdDate) }}
+                      .row.items-center
+                        q-icon.q-py-xs(color="grey-7" name="fas fa-users")
+                        .text-xs.text-h-gray.q-px-xs 56 Core members
+                      .row.items-center
+                        q-icon.q-py-xs(color="grey-7" name="fas fa-users")
+                        .text-xs.text-h-gray.q-px-xs 0 Community members
     .col-3(v-if="$q.screen.gt.md")
+      widget.sticky.bg-secondary.rounded.full-width.q-pa-md.q-mb-md(:title="'Explore by:'" :textColor="'white'")
+        .select-option(@click="displayingItemsType = 'DAOS'")
+          .select-option-label DAOs
+          .select-option-dot(:class="{ 'selected': displayingItemsType === 'DAOS' }")
+        .select-option(@click="displayingItemsType = 'ECOSYSTEMS'")
+          .select-option-label Ecosystems
+          .select-option-dot(:class="{ 'selected': displayingItemsType === 'ECOSYSTEMS' }")
       filter-widget.sticky(
         :debounce="1000"
         :defaultOption="1",
@@ -228,4 +303,31 @@ q-page.page-explore
 
 </template>
 <style lang="stylus" scoped>
+.select-option
+  display: flex
+  align-items: center
+  justify-content: space-between
+  margin-top: 15px
+  cursor: pointer
+  .select-option-label
+    color: white
+    font-size: 14px
+    font-weight: 600
+  .select-option-dot
+    display: flex
+    justify-content: center
+    align-items: center
+    border-radius: 50%
+    width: 22px
+    height: 22px
+    opacity: 0.18
+    background: #fff
+  .selected
+    opacity: 1
+  .selected::before
+    content: ''
+    background: #3F64EE
+    width: 10px
+    height: 10px
+    border-radius: 50%
 </style>
