c2c06ca203a4ea8c99974729a23ec9fec55a3176
Evgeni B <glum_psyche@gofree.dev>
2023-04-03 10:24:31 +0300

    feat(version-history): added missed icons (#2105)

--- src/components/proposals/version-history.vue
+++ src/components/proposals/version-history.vue
@@ -26,8 +26,10 @@ export default {
   .h-h7.q-mb-md Version History
   .row
     template(v-for="card in history")
-      div.bg-internal-bg.q-mr-md.q-mb-md.rounded-border.q-pa-md(:style="{ 'min-width': '160px' }" :class="{ 'selected-card': currentPeriod(card.date) }")
-        div(:style="{ 'border-radius': '50%', 'background': '#242F5D', 'width': '30px', 'height': '30px', 'display': 'flex' }" :class="{ 'selected-icon': currentPeriod(card.date) }")
+      div.bg-internal-bg.q-mr-md.q-mb-md.rounded-border.q-pa-md(:style="{ 'min-width': '160px', 'position': 'relative' }" :class="{ 'selected-card': currentPeriod(card.date) }")
+        q-icon.absolute(v-if="!currentPeriod(card.date)" name="fas fa-arrow-right" :style="{ 'right': '20px', 'top': '28px' }")
+        div(:style="{ 'border-radius': '50%', 'background': '#242F5D', 'width': '30px', 'height': '30px', 'display': 'flex', 'justify-content': 'center', 'align-items': 'center' }" :class="{ 'selected-icon': currentPeriod(card.date) }")
+          q-icon(name="fas fa-check" :color="currentPeriod(card.date) ? 'primary' : 'white'")
         .h-h5.text-weight-700.q-mt-sm.q-mb-md(:class="{ 'selected-title': currentPeriod(card.date) }") {{ card.title }}
         div(v-if="currentPeriod(card.date)") Current - on voting
         div(v-else) {{ parsedDate(card.date) }}
