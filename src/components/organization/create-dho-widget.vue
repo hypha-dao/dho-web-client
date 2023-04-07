0979846f81f7ab5c6530b9b9db54253848f7b7dc
Arsenije Savic <arsenijesavic@me.com>
2023-04-07 09:49:42 -0600

    style(create-dho-widget): add todo for copy

--- src/components/organization/create-dho-widget.vue
+++ src/components/organization/create-dho-widget.vue
@@ -10,6 +10,7 @@ export default {
 
 <template lang="pug">
 widget.q-my-md.position-relative(title="Create new DAO")
+  //- TODO: Add real copy
   //- .text-ellipsis.text-body.leading-loose.q-my-md Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
   q-btn.full-width.q-px-lg(rounded color="primary" no-caps @click="$emit('create')") Create New DAO
 </template>
