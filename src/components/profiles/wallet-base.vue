6e737fd8310438f70eecbac07a8525f5b9df6d2d
Evgeni B <glum_psyche@gofree.dev>
2023-05-03 13:12:48 +0300

    feat(payout-modal): added ui for payout modal - first step

--- src/components/profiles/wallet-base.vue
+++ src/components/profiles/wallet-base.vue
@@ -41,8 +41,14 @@ export default {
       form: {
         amount: null
       },
+      walletForm: {
+        eosAccount: null,
+        eosMemo: null
+      },
       redeem: false,
-      submitting: false
+      submitting: false,
+      showPayoutModal: false,
+      editable: false
     }
   },
 
@@ -129,6 +135,10 @@ export default {
     onSetRedemptionAddr () {
       this.resetForm()
       this.$emit('set-redeem')
+    },
+
+    formatCurrency(amount) {
+      return new Intl.NumberFormat('en-US').format(parseInt(amount), { style: 'currency' })
     }
   }
 }
@@ -136,22 +146,114 @@ export default {
 
 <template lang="pug">
 widget.wallet-base(:more="more" :no-title="noTitle" morePosition="top" title="Wallet" @more-clicked="$router.push({ path: `/${$route.params.dhoname}/@${username}/wallet` })")
-  .row.justify-center.q-mb-md(v-if="!wallet || wallet.length === 0")
-    loading-spinner(v-if="loading" color="primary" size="40px")
-    .h-b2(v-else) No wallet found
-  q-list.q-mt-md(v-else dense)
-    template(v-for="(item, index) in wallet")
-      q-item(v-if="item" :key="item.label" :class="index !== wallet.length - 1 ? 'q-mb-sm' : ''").wallet-item
-        q-item-section.icon-section(avatar)
-          token-logo(size='sm' :type="item.type" :daoLogo="daoLogo" :customIcon="item.icon")
-        q-item-section
-          q-item-label.h-b2 {{ item.label }}
-        q-item-section(side)
-          .row
-            q-item-label
-              .h-b2.text-right.text-bold.value-text {{ getFormatedTokenAmount(item.value) + (item.percentage ? ' (' + item.percentage + '%)' : '') }}
-                q-tooltip(:content-style="{ 'font-size': '1em' }" anchor="top middle" self="bottom middle") {{ getFormatedTokenAmount(item.value, Number.MAX_VALUE) }}
-    .redeem-section.q-pt-xs(v-if="canRedeem")
+  q-dialog(:value="showPayoutModal" @hide="showPayoutModal = false")
+    widget.full-width.q-pa-md(title="Redeem HUSD")
+      .row.q-mt-md
+        .col-6
+          .h-b2.text-bold.text-black.q-mb-sm HUSD available
+          .row.no-wrap.items-center
+            q-avatar.q-mr-sm(size="40px")
+              img(:style="{ 'width': '40px', 'height': '40px' }" src="~/assets/icons/usd.png")
+            div.flex.items-center.full-width(:style="{ 'background': '#F1F1F3', 'border-radius': '15px', 'padding': '12px 15px' }")
+              div {{ formatCurrency(30000) }}
+        .col-6.full-width
+      .row.q-mt-md
+        .h-b2.text-bold.text-black.q-mb-sm Amount to redeem
+        q-input.text-black.full-width(
+          v-model="form.amount"
+          type="number"
+          rounded
+          outlined
+          dense
+          placeholder="Type Amount"
+        )
+        .row.justify-end.full-width.q-mt-xs
+          .h-b2.text-primary.text-bold.text-underline(@click="form.amount = 500") 500.00
+          .h-b2.text-primary.text-bold.text-underline.q-ml-sm(@click="form.amount = 1000") 1000.00
+          .h-b2.text-primary.text-bold.text-underline.q-ml-sm(@click="form.amount = pegToken.amount") Max Available
+      .row.q-mt-md
+        .h-b2.text-bold.text-black.q-mb-sm Redeem to Address
+        div.flex.items-center.full-width(:style="{ 'background': '#F1F1F3', 'border-radius': '15px', 'padding': '20px' }")
+          .col
+            .h-b2.text-bold.text-black.q-mb-sm EOS
+            .row.no-wrap.items-center
+              q-avatar.q-mr-sm(size="40px")
+                img(:style="{ 'width': '40px', 'height': '40px' }" src="~/assets/icons/chains/eos.svg")
+              .col.q-mr-sm
+                q-input.bg-white.full-width.rounded-border(
+                  dense
+                  outlined
+                  placeholder="Account"
+                  ref="eosMemo"
+                  type = "text"
+                  v-model="walletForm.eosAccount"
+                  :disable= "!editable"
+                )
+              .col
+                q-input.bg-white.full-width.rounded-border(
+                  dense
+                  outlined
+                  placeholder="Memo"
+                  ref="eosMemo"
+                  type = "text"
+                  v-model="walletForm.eosMemo"
+                  :disable= "!editable"
+                )
+        .row.justify-end.full-width.q-mt-xs.items-center
+          template(v-if="!editable")
+            .h-b2.text-primary.text-bold.text-underline.q-mr-xs(@click="editable = true") Edit Address
+            q-icon(name="fas fa-pen" size="12px" color="primary")
+          template(v-else)
+            .h-b2.text-primary.text-bold.text-underline.q-mr-xs(@click="editable = false") Save
+        .row.full-width.q-mt-xl.no-wrap
+          .col.q-mr-sm
+            q-btn.h-btn1.full-width(
+              color="primary"
+              outline
+              no-caps
+              unelevated
+              rounded
+              :label= "'Close'"
+              :loading="submitting"
+              @click="showPayoutModal = false"
+            )
+          .col
+            q-btn.h-btn1.full-width(
+              :disable="!form.amount"
+              color="primary"
+              no-caps
+              unelevated
+              rounded
+              :label= "'Redeem HUSD'"
+              :loading="submitting"
+              @click="onRedeemHusd()"
+            )
+  .row.q-pt-xxs
+    q-btn.h-btn1.full-width(
+      color="primary"
+      no-caps
+      unelevated
+      rounded
+      :label= "'Redeem '"
+      @click="showPayoutModal = true"
+    )
+      q-tooltip(:content-style="{ 'font-size': '1em' }" anchor="top middle" self="bottom middle") Queue HUSD Redemption for Treasury Payout to Configured Wallet
+  //- .row.justify-center.q-mb-md(v-if="!wallet || wallet.length === 0")
+  //-   loading-spinner(v-if="loading" color="primary" size="40px")
+  //-   .h-b2(v-else) No wallet found
+  //- q-list.q-mt-md(v-else dense)
+  //-   template(v-for="(item, index) in wallet")
+  //-     q-item(v-if="item" :key="item.label" :class="index !== wallet.length - 1 ? 'q-mb-sm' : ''").wallet-item
+  //-       q-item-section.icon-section(avatar)
+  //-         token-logo(size='sm' :type="item.type" :daoLogo="daoLogo" :customIcon="item.icon")
+  //-       q-item-section
+  //-         q-item-label.h-b2 {{ item.label }}
+  //-       q-item-section(side)
+  //-         .row
+  //-           q-item-label
+  //-             .h-b2.text-right.text-bold.value-text {{ getFormatedTokenAmount(item.value) + (item.percentage ? ' (' + item.percentage + '%)' : '') }}
+  //-               q-tooltip(:content-style="{ 'font-size': '1em' }" anchor="top middle" self="bottom middle") {{ getFormatedTokenAmount(item.value, Number.MAX_VALUE) }}
+    //- .redeem-section.q-pt-xs(v-if="canRedeem")
       .row-md.justify-center
         q-input.full-width.rounded-border(
           :rules="[rules.greaterThan(0), rules.lessOrEqualThan(pegToken.amount)]"
