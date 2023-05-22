1d5bb49548752f0364a149a6f00e2a074b653393
Evgeni B <evgeni.biryuk.hypha@gmail.com>
2023-05-22 19:46:56 +0300

    fix(explore): hide search bar in explore page (#DEV-695) (#2180)
    * fix(explore): hide search bar in explore page (#DEV-695)
    
    * fix(search): remove search box from all pages

--- src/layouts/MultiDhoLayout.vue
+++ src/layouts/MultiDhoLayout.vue
@@ -136,7 +136,16 @@ export default {
     },
 
     loadingAccount () { return localStorage?.getItem('autoLogin') && !this.account },
-    showTopBarItems () { return this.$route.name !== 'dao-launcher' }
+    showTopBarItems () {
+      // TODO: commented out until there is a better way to make general search work
+
+      // const exceptions = ['dao-launcher', 'explore']
+      // if (exceptions.includes(this.$route.name)) {
+      //   return false
+      // }
+      // return true
+      return false
+    }
   },
 
   methods: {
@@ -266,7 +275,7 @@ q-layout(:style="{ 'min-height': 'inherit' }" :view="'lHr Lpr lFr'" ref="layout"
           )
   //- dho-switcher.fixed-left
   q-header.bg-white(v-if="$q.screen.lt.lg")
-    top-navigation(:profile="profile" @toggle-sidebar="!$q.screen.md ? right = true : showMinimizedMenu = true" @search="onSearch" :dho="dho" :dhos="getDaos($apolloData.data.member)" :selectedDaoPlan="selectedDaoPlan")
+    top-navigation(:showTopButtons="showTopBarItems" :profile="profile" @toggle-sidebar="!$q.screen.md ? right = true : showMinimizedMenu = true" @search="onSearch" :dho="dho" :dhos="getDaos($apolloData.data.member)" :selectedDaoPlan="selectedDaoPlan")
   q-page-container.bg-white.window-height.q-py-md(:class="{ 'q-pr-md': $q.screen.gt.md, 'q-px-xs': !$q.screen.gt.md}")
     .bg-internal-bg.content.full-height
       q-resize-observer(@resize="onContainerResize")
