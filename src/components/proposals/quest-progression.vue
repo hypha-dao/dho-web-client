d17da8cc011f6f9cacfecce49ee78e8f56786561
Evgeni B <glum_psyche@gofree.dev>
2023-06-09 12:55:14 +0300

    fix(quest-progression): claim quest payment request (#DEV-675)

--- src/components/proposals/quest-progression.vue
+++ src/components/proposals/quest-progression.vue
@@ -16,26 +16,37 @@ export default {
   },
 
   apollo: {
-    progression: {
+    claimPayments: {
+      query: require('~/query/quests/dao-quest-complete-info.gql'),
+      update: data => {
+        const completions = data.queryQuestcomplet.map(payment => {
+          return {
+            status: payment?.details_state_s,
+            rejectedDate: payment?.updatedDate,
+            approvedDate: payment?.system_originalApprovedDate_t,
+            title: payment?.details_title_s
+          }
+        })
+        return completions
+      },
+      skip () { return !this.proposalId },
+      variables () {
+        return {
+          id: this.proposalId
+        }
+      }
+    },
+    currentQuest: {
       query: require('~/query/quests/dao-quest-progression.gql'),
 
       update: data => {
-        const currentQuest = data.queryQueststart[0]
+        const currentQuest = data.queryQueststart?.[0]
 
         return {
-          currentQuest: {
-            status: currentQuest?.details_state_s,
-            rejectedDate: currentQuest?.updatedDate,
-            approvedDate: currentQuest?.system_originalApprovedDate_t,
-            title: currentQuest?.details_title_s
-          },
-          ascendants: currentQuest?.ascendant.map(ascendant => {
-            return {
-              title: ascendant.details_title_s,
-              status: ascendant.details_state_s,
-              approvedDate: ascendant.system_originalApprovedDate_t
-            }
-          })
+          status: currentQuest?.details_state_s,
+          rejectedDate: currentQuest?.updatedDate,
+          approvedDate: currentQuest?.system_originalApprovedDate_t,
+          title: currentQuest?.details_title_s
         }
       },
       skip () { return !this.selectedDao?.docId },
@@ -94,16 +105,25 @@ export default {
       return status
     },
     completionCardTitle () {
-      return this.progression.currentQuest.status === PROPOSAL_STATE.PROPOSED ? 'Not available yet' : 'Create now!'
+      return this.currentQuest.status === PROPOSAL_STATE.PROPOSED ? 'Not available yet' : 'Create now!'
     }
   }
 }
 </script>
 <template lang="pug">
-.quest-progression(v-if="progression.currentQuest.status !== PROPOSAL_STATE.DRAFTED")
+.quest-progression(v-if="currentQuest.status !== PROPOSAL_STATE.DRAFTED")
   .text-grey.text-italic.q-mb-sm(:style="{ 'font-size': '12px' }") Quest Progression
   .row
-    template(v-if="progression.ascendants" v-for="card in progression.ascendants")
+    div.q-mr-md.q-mb-md.rounded-border.q-pa-md(
+      :style="{ 'min-width': '160px', 'max-width': '160px' }"
+      :class="cardColor(currentQuest.status)"
+    )
+      .icon-container(:class="{ 'bg-primary': currentQuest.status === PROPOSAL_STATE.PENDING }")
+        q-icon(name="fa fa-map-marker" :color="iconColor(currentQuest.status)")
+      .h-h5.text-white(:style="{ 'white-space': 'nowrap', 'text-overflow': 'ellipsis', 'overflow': 'hidden' }") {{ currentQuest.title }}
+        q-tooltip {{ currentQuest.title }}
+      .h-h7.text-white.q-mt-md(:class="{ 'text-grey': currentQuest.status === PROPOSAL_STATE.PENDING}") {{ cardStatus(currentQuest) }}
+    template(v-if="claimPayments?.length" v-for="card in claimPayments")
       div.q-mr-md.q-mb-md.rounded-border.q-pa-md(
         :style="{ 'min-width': '160px', 'max-width': '160px' }"
         :class="cardColor(card.status)"
@@ -114,15 +134,7 @@ export default {
           q-tooltip {{ card.title }}
         .h-h7.text-white.q-mt-md(:class="{ 'text-grey': card.status === PROPOSAL_STATE.PENDING}") {{ cardStatus(card) }}
     div.q-mr-md.q-mb-md.rounded-border.q-pa-md(
-      :style="{ 'min-width': '160px', 'max-width': '160px' }"
-      :class="cardColor(progression.currentQuest.status)"
-    )
-      .icon-container(:class="{ 'bg-primary': progression.currentQuest.status === PROPOSAL_STATE.PENDING }")
-        q-icon(name="fa fa-map-marker" :color="iconColor(progression.currentQuest.status)")
-      .h-h5.text-white(:style="{ 'white-space': 'nowrap', 'text-overflow': 'ellipsis', 'overflow': 'hidden' }") {{ progression.currentQuest.title }}
-        q-tooltip {{ progression.currentQuest.title }}
-      .h-h7.text-white.q-mt-md(:class="{ 'text-grey': progression.currentQuest.status === PROPOSAL_STATE.PENDING}") {{ cardStatus(progression.currentQuest) }}
-    div.q-mr-md.q-mb-md.rounded-border.q-pa-md(
+      v-if="!claimPayments?.length"
       :style="{ 'min-width': '160px', 'max-width': '160px' }"
       :class="cardColor('pending')"
     )
