4e07a5c17412aa056578592e181c41c0008e3385
Arsenije Savic <arsenijesavic@me.com>
2023-07-12 08:27:34 -0600

    refactor(proposal-create): add const to proposal type

--- src/pages/proposals/ProposalCreate.vue
+++ src/pages/proposals/ProposalCreate.vue
@@ -294,7 +294,7 @@ export default {
 
     refer (obj) {
       this.reference = obj
-      if (this.selectedConfig.type === 'Assignment Badge') {
+      if (this.selectedConfig.type === PROPOSAL_TYPE.ABILITY) {
         this.$store.commit('proposals/setBadge', this.reference)
         this.$store.commit('proposals/setRewardCoefficientLabel', (this.reference.details_rewardCoefficientX10000_i) / 10000)
         this.$store.commit('proposals/setRewardCoefficient', this.reference.details_rewardCoefficientX10000_i)
@@ -303,7 +303,7 @@ export default {
         this.$store.commit('proposals/setPegCoefficientLabel', (this.reference.details_pegCoefficientX10000_i) / 10000)
         this.$store.commit('proposals/setPegCoefficient', this.reference.details_pegCoefficientX10000_i)
         this.$store.commit('proposals/setIcon', this.reference.details_icon_s)
-      } else if (this.selectedConfig.type === 'Policy') {
+      } else if (this.selectedConfig.type === PROPOSAL_TYPE.POLICY) {
         if (this.reference.value !== '') {
           this.$store.commit('proposals/setMasterPolicy', this.reference)
         } else {
