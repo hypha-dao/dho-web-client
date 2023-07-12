65953a667fbd06718099de5250caf3e70383addb
Evgeni B <glum_psyche@gofree.dev>
2023-07-12 12:28:43 +0300

    fix(design): copy UI changes

--- src/pages/proposals/create/StepPayout.vue
+++ src/pages/proposals/create/StepPayout.vue
@@ -388,8 +388,10 @@ widget(:class="{ 'disable-step': currentStepName !== 'step-payout' && $q.screen.
     label.h-label(v-if="$store.state.proposals.draft.annualUsdSalary.toString().includes('USD')") {{ $t('pages.proposals.create.steppayout.salaryCompensationForOneYear', { value: $store.state.proposals.draft.annualUsdSalary }) }}
     label.h-label(v-else) {{ $t('pages.proposals.create.steppayout.salaryCompensationForOneYearUsd', { value: $store.state.proposals.draft.annualUsdSalary }) }}
   .row.q-mt-xxxl(v-if="$q.screen.gt.md")
-    label.h-h4 {{ $t('pages.proposals.create.steppayout.compensation') }}
-    .text-body2.text-grey-7.q-my-md {{ $t('pages.proposals.create.steppayout.pleaseEnterTheUSD') }}
+    label.h-h4(v-if="$store.state.proposals.draft.type === 'Assignment'") {{ $t('pages.proposals.create.steppayout.compensation1') }}
+    label.h-h4(v-else) {{ $t('pages.proposals.create.steppayout.compensation') }}
+    .text-body2.text-grey-7.q-my-md.full-width(v-if="$store.state.proposals.draft.type === 'Assignment'") {{ $t('pages.proposals.create.steppayout.pleaseEnterTheUSDEquivalentAnd1') }}
+    .text-body2.text-grey-7.q-my-md.full-width(v-else) {{ $t('pages.proposals.create.steppayout.belowYouCanSeeTheActual') }}
   .row(v-if="isAssignment")
     label.text-bold {{ toggle ? $t('pages.proposals.create.steppayout.compensationForOnePeriod') : $t('pages.proposals.create.steppayout.compensationForOneCycle') }}
   .q-col-gutter-xs.q-mt-sm(:class="{ 'q-mt-xxl':$q.screen.lt.md || $q.screen.md, 'row':$q.screen.gt.md }")
