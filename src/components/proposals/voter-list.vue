d28694eec8226165add52cb01b627fc2576f852d
Arsenije Savić <arsenijesavic@me.com>
2023-07-13 09:07:57 -0600

    refactor(voter-list): clean up (#2322)

--- src/components/proposals/voter-list.vue
+++ src/components/proposals/voter-list.vue
@@ -1,140 +1,215 @@
 <script>
-import { date } from 'quasar'
+import { mapActions } from 'vuex'
+import gql from 'graphql-tag'
+
+const PROPOSAL_VOTES_QUERY = `
+  getDocument(docId: $docId) {
+    __typename
+    docId
+
+    ... on Votable {
+      __typename
+
+      voteAggregate {
+        count
+      }
+
+      vote(first: $first, offset: $offset, order: { desc: createdDate }) {
+        username: vote_voter_n
+        vote: vote_vote_s
+        strength: vote_votePower_a
+      }
+    }
+  }
+`
 
 export default {
   name: 'voter-list',
   components: {
     Chips: () => import('~/components/common/chips.vue'),
     ProfilePicture: () => import('~/components/profiles/profile-picture.vue'),
-    Widget: () => import('~/components/common/widget.vue'),
-    LoadingSpinner: () => import('~/components/common/loading-spinner.vue')
+    Widget: () => import('~/components/common/widget.vue')
   },
 
   props: {
-    votes: {
-      type: Array,
-      default: () => []
-    },
-    size: {
-      type: Number,
-      default: 0
-    }
-  },
-
-  computed: {
-    pages () {
-      return Math.ceil(this.size / 5)
-    },
-
-    paginatedVotes () {
-      return this.votes.slice((this.page - 1) * 5, this.page * 5)
-    },
-    getPaginationText () {
-      if (this.pages === 0) return ''
-      return `${this.page} of ${this.pages}`
-    },
-    isLastPage () {
-      if (this.pages === 0) return true
-      return this.page === this.pages
-    },
-    iconPercentage () {
-      return require('~/assets/icons/voice.svg')
-    }
+    proposalId: String
   },
 
   data () {
     return {
-      page: 1
+      page: 0,
+      size: 5,
+      voices: {},
+      voteCount: 0
+    }
+  },
+
+  apollo: {
+    votes: {
+      query: gql`query proposalVotes($docId: String!, $first: Int!, $offset: Int!) { ${PROPOSAL_VOTES_QUERY} }`,
+      update: data => data.getDocument.vote,
+      skip () { return !this.proposalId },
+      variables () {
+        return {
+          docId: this.proposalId,
+          first: this.size,
+          offset: this.page * this.size
+        }
+      },
+
+      result ({ data }) {
+        this.voteCount = data?.getDocument?.voteAggregate?.count
+      },
+
+      subscribeToMore: {
+        document: gql`subscription proposalVotes($docId: String!, $first: Int, $offset: Int) { ${PROPOSAL_VOTES_QUERY} }`,
+        skip () { return !this.proposalId },
+        variables () { return { docId: this.proposalId } },
+        updateQuery: (previousResult, { subscriptionData }) => {
+          if (!subscriptionData.data) {
+            return previousResult
+          }
+          if (!previousResult) {
+            return undefined
+          }
+
+          return subscriptionData.data
+        }
+
+      }
+
+    }
+  },
+
+  computed: {
+    isFirstPage () { return this.pageCount === 0 ? true : this.page + 1 === 1 },
+    isLastPage () { return this.pageCount === 0 ? true : this.page + 1 === this.pageCount },
+    isLoading () { return this.$apollo.queries.votes.loading },
+    pageCount () { return Math.ceil(this.voteCount / 5) },
+
+    votesPaginated () {
+      if (!this.votes) return []
+      return [...this.votes].splice(this.page * this.size, this.size)
     }
   },
 
   methods: {
-    color (vote) {
-      if (vote.vote === 'pass') return 'positive'
-      if (vote.vote === 'fail') return 'negative'
-      return 'disabled'
-    },
-
-    icon (vote) {
-      if (vote.vote === 'pass') return 'fas fa-check-circle'
-      if (vote.vote === 'fail') return 'fas fa-times-circle'
-      if (vote.vote === 'abstain') return 'fas fa-minus-circle'
-      return 'fas fa-minus-circle'
-    },
+    ...mapActions('profiles', ['getVoiceToken']),
+    ...mapActions('treasury', { getTreasurySupply: 'getSupply' }),
 
     tag (vote) {
       if (vote.vote === 'pass') {
         return {
           label: 'Yes',
-          color: 'positive',
-          dense: true
+          color: 'positive'
         }
       }
       if (vote.vote === 'fail') {
         return {
           label: 'No',
-          color: 'negative',
-          dense: false
+          color: 'negative'
         }
       }
       if (vote.vote === 'abstain') {
         return {
           label: 'Abstain',
-          color: 'disabled',
-          dense: true
+          color: 'disabled'
         }
       }
-      return null
+      return { label: '', color: '' }
     },
-    voteDate (vote) {
-      const now = new Date()
-      const voteDay = new Date(vote.date)
 
-      const days = date.getDateDiff(now, voteDay, 'days')
-      if (days > 1) return `${days} days ago`
-      if (days === 1) return `${days} day ago`
+    async loadVoiceTokenPercentage ({ username, strength }) {
+      // TODO: Check if this is edge case or bad code from past
+      //   if (this.proposal && this.proposal.details_ballotSupply_a) {
+      //       const [supplyAmount, token] = this.proposal.details_ballotSupply_a.split(' ')
+      //       const percentage = calcVoicePercentage(vote.vote_votePower_a.split(' ')[0], supplyAmount)
+      //   } else {
 
-      const hours = date.getDateDiff(now, voteDay, 'hours')
-      if (hours > 1) return `${hours} hours ago`
-      if (hours === 1) return `${hours} hour ago`
+      const [supplyTokens, voiceToken] = await Promise.all([
+        this.getTreasurySupply(),
+        this.getVoiceToken(username)
+      ])
+      const supplyHVoice = parseFloat(supplyTokens[voiceToken.token])
+      const userHVoice = parseFloat(voiceToken.amount)
 
-      const minutes = date.getDateDiff(now, voteDay, 'minutes')
-      if (minutes > 1) return `${minutes} minutes ago`
-      if (minutes === 1) return `${minutes} minute ago`
+      if (supplyHVoice && userHVoice) {
+        // TODO: Check if this is edge case or bad code from past
+        // if (parseFloat(voiceToken.amount) === parseFloat(strength)) {
+        //   percentage = supplyHVoice ? calcVoicePercentage(parseFloat(voiceToken.amount), supplyHVoice) : '0.0'
+        // } else {
+        //   percentage = supplyHVoice ? calcVoicePercentage(parseFloat(strength), supplyHVoice) : '0.0'
+        // }
 
-      return '1 minute ago'
-    },
-    load () {
-      if (this.isLastPage) return
-      this.$emit('onload')
-      this.page++
-    },
-    onPrev () {
-      this.page--
-    },
-    imageUrl () {
-      return require('~/assets/icons/voice.svg')
+        const percent = parseFloat((userHVoice / supplyHVoice) * 100).toFixed(2)
+
+        this.$set(this.voices, username, percent > 0.1 ? percent : 0.1)
+      }
     }
+  },
+
+  watch: {
+    votes: {
+      immediate: true,
+      deep: true,
+      async handler (votes) {
+        if (votes) {
+          const loadedVoters = Object.keys(this.voices)
+          const notLoadedVoters = votes.filter(_ => !loadedVoters.includes(_.username))
+          await Promise.all(notLoadedVoters.map(this.loadVoiceTokenPercentage))
+        }
+      }
+    }
+
   }
+
 }
 </script>
 
 <template lang="pug">
-widget(:title="$t('proposals.voter-list.votes')")
-  .h-h2.text-secondary.absolute(:style="{ 'top': '-10px', 'right': '0' }") {{ size }}
-  template(v-if="(paginatedVotes.length === 0) && (size !== 0)")
-    .row.justify-center.q-my-md
-      loading-spinner(color="primary" size="40px")
-  template(v-for="vote of paginatedVotes")
+widget(:title="$t('proposals.voter-list.votes')" bar)
+  template(v-slot:header)
+    .h-h2.text-secondary {{ voteCount }}
+
+  template(v-for="vote of votesPaginated")
     .row.items-center.justify-between.q-my-md(:key="vote.username")
-      profile-picture(:username="vote.username" show-name size="40px" limit link noMargins ellipsisName)
+      profile-picture(
+        :username="vote.username"
+        limit
+        link
+        noMargins
+        show-name
+        size="40px"
+      )
         template(v-slot:detail)
-          .h-b3.text-italic.text-grey-6 {{ vote.percentage }}
-      chips(:tags="[tag(vote)]")
-      // q-icon(:name="icon(vote)" :color="color(vote)" size="sm")
-  .row.justify-between.q-pt-sm.items-center
-    q-btn.round-circle(@click="onPrev()" :disable="page === 1" round unelevated icon="fas fa-chevron-left" color="inherit" text-color="primary" size="sm" :ripple="false")
-    span {{  getPaginationText }}
-    q-btn.round-circle(@click="load" :disable="isLastPage" round unelevated icon="fas fa-chevron-right" color="inherit" text-color="primary" size="sm" :ripple="false")
+          .h-b3.text-italic.text-grey-6.q-pa-none.q-ma-none
+            span(v-if="voices[vote.username]") {{ voices[vote.username] }}% HVOICE
+            q-skeleton(v-else type="text")
+      q-chip.h-tag.tag(dense text-color="white" v-bind="{...tag(vote)}")
+
+  nav.row.justify-between(v-if="voteCount > 0")
+    q-btn.round-circle(
+      :disable="isFirstPage"
+      :ripple="false"
+      @click="page--"
+      color="inherit"
+      icon="fas fa-chevron-left"
+      round
+      size="sm"
+      text-color="primary"
+      unelevated
+    )
+    span(v-if="pageCount > 0") {{ page + 1 }} of {{ pageCount }}
+    q-btn.round-circle(
+      :disable="isLastPage"
+      :ripple="false"
+      @click="page++"
+      color="inherit"
+      icon="fas fa-chevron-right"
+      round
+      size="sm"
+      text-color="primary"
+      unelevated
+    )
 
 </template>
