8d2eaa2b15447df7a905fbdbd5bcadfbd50288ec
NIK <nheger@gmail.com>
2024-10-09 16:39:42 +0800

    fix: add isCreateDao

--- src/components/login/welcome-view.vue
+++ src/components/login/welcome-view.vue
@@ -12,12 +12,15 @@ export default {
   },
 
   computed: {
-    ...mapGetters('dao', ['daoSettings', 'selectedDao'])
+    ...mapGetters('dao', ['daoSettings', 'selectedDao']),
+    isCreateDao() {
+      return this.$router.currentRoute.name === 'create-your-dao'
+    }
   },
 
   methods: {
     goNext() {
-      if (this.$router.currentRoute.name === 'create-your-dao' && localStorage.getItem('autoLogin')) {
+      if (this.isCreateDao && localStorage.getItem('autoLogin')) {
         this.$emit('onCreateDaoClick')
       } else {
         this.$emit('onLoginClick')
@@ -30,7 +33,7 @@ export default {
 .full-width.full-height.flex(:class="{ 'items-start q-pa-xl':!$q.screen.gt.md, 'items-center':$q.screen.gt.md }")
   .full-width.full-height.flex.column.justify-between.no-wrap(:style="{ 'padding-bottom': '50px' }")
     .full-height.flex.column(:class="{ 'justify-start': !$q.screen.gt.md, 'justify-center': $q.screen.gt.md }")
-      .row(v-if="$router.currentRoute.name === 'create-your-dao'" :class="{ 'q-mb-xxxl': $q.screen.gt.md }")
+      .row(v-if="isCreateDao" :class="{ 'q-mb-xxxl': $q.screen.gt.md }")
         .font-lato.text-heading.text-bold(:class="{ 'desktop-font-size': $q.screen.gt.md }" :style="{ 'font-size': '38px' }") {{ $t('login.welcome-view.launchYourFirst') }}
         .text-heading.q-mt-md.text-bold(:style="'font-size: 18px'") {{ $t('login.welcome-view.youNeedAHyphaAccountPangea') }}
       .row(v-else :style="'line-height: 52px; width: 70%;'" :class="{ 'q-mb-xxxl': $q.screen.gt.md }")
@@ -38,7 +41,7 @@ export default {
         .font-lato.text-heading.text-bold(:class="{ 'desktop-font-size': $q.screen.gt.md }" :style="{ 'font-size': '38px' }") {{ isOnboarding ? 'first DAO' : selectedDao?.title?.replace(/^\w/, (c) => c.toUpperCase()) }}
       .h-h4.color-secondary.q-my-xxxl(v-if="$q.screen.gt.md && isOnboarding") {{ $t('login.welcome-view.youNeedAn') }}
       .h-b1-signup.text-h-gray.q-py-xxs.leading-loose(v-if="$q.screen.gt.md && !isOnboarding")
-        template(v-if="$router.currentRoute.name === 'create-your-dao'") {{ $t('login.welcome-view.ifYouAlreadyHaveAHyphaAccountPangea') }}
+        template(v-if="isCreateDao") {{ $t('login.welcome-view.ifYouAlreadyHaveAHyphaAccountPangea') }}
         template(v-else) {{ $t('login.welcome-view.isonboardingPangea') }}
       .h-b1-signup.color-secondary.q-mt-lg.q-mb-lg.text-grey.custom-calc-margin(v-if="!$q.screen.gt.md") {{ $t('login.welcome-view.theDhoDecentralized') }}
     div
@@ -56,7 +59,7 @@ export default {
         div {{$t('login.welcome-view.login')}}
         div
       </q-avatar>
-    q-btn.full-width.q-mt-sm.text-bold(:to="{ name:'dashboard' }" color="primary" :label="$t('login.welcome-view.continueAsAGuest')" no-caps outline rounded unelevated v-if="!isOnboarding && $router.currentRoute.name !== 'create-your-dao'")
+    q-btn.full-width.q-mt-sm.text-bold(:to="{ name:'dashboard' }" color="primary" :label="$t('login.welcome-view.continueAsAGuest')" no-caps outline rounded unelevated v-if="!isOnboarding && isCreateDao")
       //- TODO: temporarily commented (what should happen after clicking)
       //- .h-b1-signup.text-h-gray.q-py-xl.row.items-center {{ $t('login.welcome-view.useAnExisting') }}
       //-   q-btn.q-pa-none.text-underline.q-ml-xxs(flat color="primary" no-caps padding="none" @click="onPreviousStep") {{ $t('login.welcome-view.blockhainAccount') }}
