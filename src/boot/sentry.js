5eba9b815efc8c75253f1e864daf7972c3a055bb
NIK <nheger@gmail.com>
2024-10-25 22:55:06 +0800

    feat: disable sentry
    Sentry has not been working for years, also causing errors with requests.

--- src/boot/sentry.js
+++ src/boot/sentry.js
@@ -1,23 +1,24 @@
 import * as Sentry from '@sentry/vue'
-import { BrowserTracing } from '@sentry/tracing'
+// import { BrowserTracing } from '@sentry/tracing'
 
 export default async ({ Vue, store, router }) => {
   Sentry.init({
     Vue,
     dsn: process.env.SENTRY_DSN,
-    environment: process.env.CI_ENVIRONMENT_NAME,
-    release: process.env.CI_COMMIT_SHORT_SHA,
-    integrations: [
-      new BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-        tracingOrigins: ['localhost', 'hypha.earth', /^\//]
-      })
-    ],
-
+    enabled: false // Disable Sentry
+    // environment: process.env.CI_ENVIRONMENT_NAME,
+    // release: process.env.CI_COMMIT_SHORT_SHA,
+    // tracePropagationTargets: [], // Disable tracing
+    // integrations: [
+    //   new BrowserTracing({
+    //     routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    //     tracingOrigins: ['localhost', 'hypha.earth', /^\//]
+    //   })
+    // ],
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    // tracesSampleRate: 1.0
   })
 
   Vue.prototype.$sentry = Sentry
